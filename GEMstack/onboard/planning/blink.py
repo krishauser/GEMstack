acfdf57e41ed1ef7644fd0c71556376ea822ba27
yudiqin2 <yudiqin2@illinois.edu>
2024-01-29 19:32:55 -0600

    adding HW1 code

---
+++ GEMstack/onboard/planning/blink.py
@@ -0,0 +1,102 @@
+import rospy
+from std_msgs.msg import String, Bool, Float32, Float64
+from pacmod_msgs.msg import PositionWithSpeed, PacmodCmd, SystemRptInt, SystemRptFloat, VehicleSpeedRpt
+
+# For message format, see
+# https://github.com/astuff/astuff_sensor_msgs/blob/3.3.0/pacmod_msgs/msg/PacmodCmd.msg
+
+class BlinkDistress:
+    """Your control loop code should go here.  You will use ROS and Pacmod messages
+    to communicate with drive-by-wire system to control the vehicle's turn signals.
+    """
+    def __init__(self):
+        # TODO: Initialize your publishers and subscribers here
+        
+        self.sub_accel = rospy.Subscriber('parsed_tx/accel_rpt', SystemRptFloat, self.callback_accel)
+        self.sub_brake = rospy.Subscriber('parsed_tx/brake_rpt', SystemRptFloat, self.callback_brake)
+        self.pub = rospy.Publisher('/pacmod/as_rx/turn_cmd', PacmodCmd, queue_size=10)
+
+        self.signal = PacmodCmd()
+        self.signal.ui16_cmd = 1
+        # When you create a subscriber to a /pacmod/parsed_tx/X topic, you will need to provide a callback function.
+        # You will want this callback to be a BlinkDistress method, such as print_X(self, msg).  msg will have a
+        # ROS message type, and you can find out what this is by either reading the documentation or running
+        # "rostopic info /pacmod/parsed_tx/X" on the command line.
+        
+        # pass
+
+    def rate(self):
+        """Requested update frequency, in Hz"""
+        return 0.5
+
+    def initialize(self):
+        """Run first"""
+        pass
+
+    def cleanup(self):
+        """Run last"""
+        pass
+    
+    def update(self):
+        """Run in a loop"""
+        # TODO: Implement your control loop here
+        # You will need to publish a PacmodCmd() to /pacmod/as_rx/turn_cmd.  Read the documentation to see
+        # what the data in the message indicates.
+        if(self.signal.ui16_cmd == 1):
+            self.signal.ui16_cmd = 2
+        elif(self.signal.ui16_cmd == 2):
+            self.signal.ui16_cmd = 0
+        else:
+            self.signal.ui16_cmd = 1
+    
+        self.pub.publish(self.signal)
+        # pass
+    
+    # callbacks 
+    def callback_accel(self, data):
+        rospy.loginfo(data.data)
+
+    def callback_brake(self, data):
+        rospy.loginfo(data.data)
+       
+    def healthy(self):
+        """Returns True if the element is in a stable state."""
+        return True
+       
+    def done(self):
+        """Return True if you want to exit."""
+        return False
+
+
+
+def run_ros_loop(node):
+    """Executes the event loop of a node using ROS.  `node` should support
+    rate(), initialize(), cleanup(), update(), and done().  You should not modify
+    this code.
+    """
+    #intializes the node. We use disable_signals so that the end() function can be called when Ctrl+C is issued.
+    #See http://wiki.ros.org/rospy/Overview/Initialization%20and%20Shutdown
+    rospy.init_node(node.__class__.__name__, disable_signals=True)
+
+    node.initialize()
+    rate = rospy.Rate(node.rate())
+    termination_reason = "undetermined"
+    try:
+        while not rospy.is_shutdown() and not node.done() and node.healthy():
+            node.update()
+            rate.sleep()
+        if node.done():
+            termination_reason = "Node done"
+        if not node.healthy():
+            termination_reason = "Node in unhealthy state"
+    except KeyboardInterrupt:
+        termination_reason = "Killed by user"
+    except Exception as e:
+        termination_reason = "Exception "+str(e)
+        raise
+    finally:
+        node.cleanup()
+        rospy.signal_shutdown(termination_reason)
+
+if __name__ == '__main__':
+    run_ros_loop(BlinkDistress())
---
+++ GEMstack/onboard/planning/blink_component.py
@@ -0,0 +1,53 @@
+from ..component import Component
+from ..interface.gem import GEMInterface,GEMVehicleCommand,GEMVehicleReading
+
+
+class BlinkDistress(Component):
+    """Your control loop code should go here.  You will use GEMVehicleCommand
+    to communicate with drive-by-wire system to control the vehicle's turn signals.
+    """
+    def __init__(self, vehicle_interface : GEMInterface):
+        self.vehicle_interface = vehicle_interface
+        self.command = None
+
+    def callback(self, data):
+        rospy.loginfo(data.data)
+
+    def rate(self):
+        """Requested update frequency, in Hz"""
+        return 0.5
+
+    def initialize(self):
+        """Run first"""
+        pass
+
+    def cleanup(self):
+        """Run last"""
+        pass
+    
+    def update(self):
+        """Run in a loop"""
+        # we need to set up a GEMVehicleCommand which encapsulates all commands that will be
+        # sent to the drive-by-wire system, simultaneously.  To avoid doing arbitrary things
+        # to the vehicle, let's maintain the current values (e.g., accelerator, brake pedal,
+        # steering angle) from its current readings.
+        command = self.vehicle_interface.command_from_reading()
+        if command.left_turn_signal == False and command.right_turn_signal == False:
+            command.left_turn_signal = True
+
+        elif command.left_turn_signal == True and command.right_turn_signal == False:
+            command.left_turn_signal = False
+            command.right_turn_signal = True
+
+        elif command.right_turn_signal == True and command.left_turn_signal == False:
+            command.right_turn_signal = False
+            command.left_turn_signal = False
+
+        # TODO: alter command to execute turn signals, then uncomment line below to send
+        # the command to vehicle
+        self.vehicle_interface.send_command(command)
+       
+    def healthy(self):
+        """Returns True if the element is in a stable state."""
+        return True
+       
