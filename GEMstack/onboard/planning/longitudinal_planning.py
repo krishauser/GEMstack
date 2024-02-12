ddee28bd217ac73a5a0c23dba67592e1cecfd3de
Paul Osuma <39924481+paulosuma@users.noreply.github.com>
2024-02-12 13:43:55 -0600

    Add files via upload
    update on longitudinal planning

--- GEMstack/onboard/planning/longitudinal_planning.py
+++ GEMstack/onboard/planning/longitudinal_planning.py
@@ -1,7 +1,7 @@
 from typing import List
 from ..component import Component
 from ...state import AllState, VehicleState, EntityRelation, EntityRelationEnum, Path, Trajectory, Route, ObjectFrameEnum
-from ...utils import serialization
+from ...utils import serialization, settings
 from ...mathutils.transforms import vector_madd
 import math
 
@@ -17,29 +17,73 @@ def longitudinal_plan(path : Path, acceleration : float, deceleration : float, m
     path_normalized = path.arc_length_parameterize()
     #TODO: actually do something to points and times
 
+    points = [p for p in path_normalized.points]
+    times = [t/(path_normalized.times[-1]) for t in path_normalized.times]
+
+    total_distance = path_normalized.times[-1]
+
+    # # If acceleration is zero, use triangular profile with non-jerk for smooth transition 
+    if acceleration == 0:
+        jerk = -10 #m/s^3
+        # Calculate transition time
+        transition_time = math.sqrt(2 * abs(current_speed) / abs(jerk))
+        const_speed_time = abs(current_speed) / abs(jerk)
+        total_time = transition_time + const_speed_time
+        times = [total_time * t for t in times]
+        return Trajectory(path.frame, points, times)
+
+    if current_speed > max_speed:
+        #decelerate
+        total_time = current_speed/deceleration
+        times = [total_time * t for t in times]
+        return Trajectory(path.frame, points, times)
+    
+    decel_dist_curr_speed = current_speed**2/(2*deceleration)
+    decel_dist_max_speed = max_speed**2/(2*deceleration)
+    accel_dist_curr_to_max = (max_speed**2 - current_speed**2) / (2 * acceleration)
 
 
-    trajectory = Trajectory(path.frame,points,times)
-    return trajectory
+    #if we'll reach max speed and decelerate
+    if total_distance-decel_dist_max_speed >= accel_dist_curr_to_max:
+        accel_time = (max_speed-current_speed)/acceleration
+        decel_time = math.sqrt(2*decel_dist_max_speed/deceleration)
+        const_velocity_time = (total_distance-decel_dist_max_speed-accel_dist_curr_to_max)/max_speed
+        total_time = accel_time+decel_time+const_velocity_time
+        times = [total_time * t for t in times]
+        return Trajectory(path.frame, points, times)
+    
+
+    #still have dist to accelerate, accel
+    dt = 0.005 #seconds
+    total_time = 0.0
+    while total_distance-decel_dist_curr_speed > 0:
+        #accelerate
+        current_speed = current_speed + acceleration*dt
+        total_time += dt
+        decel_dist_curr_speed = current_speed**2/(2*deceleration)
+    total_time += math.sqrt(2*decel_dist_curr_speed/deceleration)
+    times = [total_time * t for t in times]
+    return Trajectory(path.frame, points, times)
+
+    return Trajectory(path.frame, points, times)
 
 
 def longitudinal_brake(path : Path, deceleration : float, current_speed : float) -> Trajectory:
     """Generates a longitudinal trajectory for braking along a path."""
     path_normalized = path.arc_length_parameterize()
     #TODO: actually do something to points and times
-    points = [p for p in path_normalized.points]
-    times = [t for t in path_normalized.times]
 
-    #my work
+    if path_normalized.times[-1] == 0:
+        return Trajectory(path.frame,path_normalized.points,path_normalized.times)
+    
+    points = [p for p in path_normalized.points]
+    times = [t/(path_normalized.times[-1]) for t in path_normalized.times]
+
     # Compute the time needed to decelerate to stop
     decel_time = current_speed / deceleration
 
-    # Generate trajectory with deceleration
     times = [t * decel_time for t in times] 
-
-
-    trajectory = Trajectory(path.frame,points,times)
-    return trajectory
+    return Trajectory(path.frame,points,times)
 
 
 class YieldTrajectoryPlanner(Component):
@@ -50,9 +94,9 @@ class YieldTrajectoryPlanner(Component):
     def __init__(self):
         self.route_progress = None
         self.t_last = None
-        self.acceleration = 0.5
-        self.desired_speed = 1.0
-        self.deceleration = 2.0
+        self.acceleration = settings.get('control.longitudinal_planning_yielding.acceleration')
+        self.desired_speed = settings.get('control.longitudinal_planning_yielding.desired_speed')
+        self.deceleration = settings.get('control.longitudinal_planning_yielding.deceleration')
 
     def state_inputs(self):
         return ['all']
@@ -88,7 +132,7 @@ class YieldTrajectoryPlanner(Component):
         #parse the relations indicated
         should_brake = False
         for r in state.relations:
-            if r.type == EntityRelationEnum.YIELD and r.obj1 == '':
+            if r.type == EntityRelationEnum.YIELDING and r.obj1 == '':
                 #yielding to something, brake
                 should_brake = True
         should_accelerate = (not should_brake and curr_v < self.desired_speed)
