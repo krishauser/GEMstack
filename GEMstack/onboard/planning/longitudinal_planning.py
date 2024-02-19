394222e2135a5b471a11324aa36af97f1578e95f
Andy-Chen-design <111224941+Andy-Chen-design@users.noreply.github.com>
2024-02-18 22:54:09 -0600

    Update longitudinal_planning.py 0218

--- GEMstack/onboard/planning/longitudinal_planning.py
+++ GEMstack/onboard/planning/longitudinal_planning.py
@@ -1,12 +1,3 @@
-from typing import List
-from ..component import Component
-from ...state import AllState, VehicleState, EntityRelation, EntityRelationEnum, Path, Trajectory, Route, ObjectFrameEnum
-from ...utils import serialization
-from ...mathutils.transforms import vector_madd
-import math
-import sympy
-import numpy as np
-
 def longitudinal_plan(path : Path, acceleration : float, deceleration : float, max_speed : float, current_speed : float) -> Trajectory:
     """Generates a longitudinal trajectory for a path with a
     trapezoidal velocity profile. 
@@ -287,66 +278,3 @@ def longitudinal_brake(path : Path, deceleration : float, current_speed : float)
         # traj_times = [times[0], t_dec]
     trajectory = Trajectory(path.frame,traj_points,traj_times)
     return trajectory
-
-
-class YieldTrajectoryPlanner(Component):
-    """Follows the given route.  Brakes if you have to yield or
-    you are at the end of the route, otherwise accelerates to
-    the desired speed.
-    """
-    def __init__(self):
-        self.route_progress = None
-        self.t_last = None
-        self.acceleration = 0.5
-        self.desired_speed = 1.0
-        self.deceleration = 2.0
-
-    def state_inputs(self):
-        return ['all']
-
-    def state_outputs(self) -> List[str]:
-        return ['trajectory']
-
-    def rate(self):
-        return 10.0
-
-    def update(self, state : AllState):
-        vehicle = state.vehicle # type: VehicleState
-        route = state.route   # type: Route
-        t = state.t
-
-        if self.t_last is None:
-            self.t_last = t
-        dt = t - self.t_last
-  
-        curr_x = vehicle.pose.x
-        curr_y = vehicle.pose.y
-        curr_v = vehicle.v
-
-        #figure out where we are on the route
-        if self.route_progress is None:
-            self.route_progress = 0.0
-        closest_dist,closest_parameter = state.route.closest_point_local((curr_x,curr_y),[self.route_progress-5.0,self.route_progress+5.0])
-        self.route_progress = closest_parameter
-
-        #extract out a 10m segment of the route
-        route_with_lookahead = route.trim(closest_parameter,closest_parameter+10.0)
-        print(route_with_lookahead.points)
-        #parse the relations indicated
-        should_brake = False
-        for r in state.relations:
-            if r.type == EntityRelationEnum.YIELDING and r.obj1 == '':
-                #yielding to something, brake
-                should_brake = True
-        should_accelerate = (not should_brake and curr_v < self.desired_speed)
-
-        #choose whether to accelerate, brake, or keep at current velocity
-        if should_accelerate:
-            traj = longitudinal_plan(route_with_lookahead, self.acceleration, self.deceleration, self.desired_speed, curr_v)
-        elif should_brake:
-            traj = longitudinal_brake(route_with_lookahead, self.deceleration, curr_v)
-        else:
-            traj = longitudinal_plan(route_with_lookahead, 0.0, self.deceleration, self.desired_speed, curr_v)
-        # print("traj_points = ", traj.points)
-        
-        return traj 
