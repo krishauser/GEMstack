66e52ffcc65764a91ba287af978a345eefd537f8
Ricky Liang <109532763+Ricky-Liang@users.noreply.github.com>
2024-03-08 16:53:55 -0600

    Add files via upload

--- GEMstack/onboard/planning/pedestrian_avoidance_planning.py
+++ GEMstack/onboard/planning/pedestrian_avoidance_planning.py
@@ -41,29 +41,27 @@ class PedestrianAvoidanceMotionPlanner(Component):
         if self.t_last is None:
             self.t_last = t
         dt = t - self.t_last
-
+  
         curr_x = vehicle.pose.x
         curr_y = vehicle.pose.y
         curr_v = vehicle.v    
-        curr_w = vehicle.dimensions[0]
-        curr_h = vehicle.dimensions[1]
 
-        # put the route and the agents in the vehicle's frame
+        #put the route and the agents in the vehicle's frame
         if state.route.frame != vehicle.pose.frame:
             state.route = state.route.to_frame(vehicle.pose.frame, current_pose=state.vehicle.pose, start_pose_abs=state.start_vehicle_pose)
         for k,a in agents.items():
             a.pose = a.pose.to_frame(vehicle.pose.frame, current_pose=state.vehicle.pose, start_pose_abs=state.start_vehicle_pose)
 
-        # figure out where we are on the route
+        #figure out where we are on the route
         if self.route_progress is None:
             self.route_progress = 0.0
-        closest_dist, closest_parameter = state.route.closest_point_local((curr_x,curr_y),[self.route_progress-5.0,self.route_progress+5.0])
+        closest_dist,closest_parameter = state.route.closest_point_local((curr_x,curr_y),[self.route_progress-5.0,self.route_progress+5.0])
         self.route_progress = closest_parameter
 
-        # all_pedestrians = []
-        # for k,a in agents.items():
-        #     if a.type == AgentEnum.PEDESTRIAN:
-        #         all_pedestrians.append(a)
+        all_pedestrians = []
+        for k,a in agents.items():
+            if a.type == AgentEnum.PEDESTRIAN:
+                all_pedestrians.append(a)
 
         #extract out a 10m segment of the route
         route_with_lookahead = route.trim(closest_parameter,closest_parameter+10.0)
@@ -71,20 +69,14 @@ class PedestrianAvoidanceMotionPlanner(Component):
         
         #TODO: use the collision detection primitives to determine whether to stop for a pedestrian
         #TODO: modify the margins around the vehicle to keep a safe distance from pedestrians
-        margin_lateral = 1.0
-        margin_longitudinal = 3.0
-        look_ahead_dist = (self.desired_speed ** 2) / (2 * self.deceleration) + margin_longitudinal # The distance you need to stop
-        
         all_pedestrians = []
         for k,a in agents.items():
             if a.type == AgentEnum.PEDESTRIAN:
                 all_pedestrians.append(a)
-
         collision_check_resolution = 0.1  #m
         progress_start, progress_end = route_with_lookahead.domain()
         progress = progress_start
         max_progress = None
-
         while progress < progress_end:
             xy = route_with_lookahead.eval(progress)
             vehicle.pose.x = xy[0]
@@ -99,31 +91,30 @@ class PedestrianAvoidanceMotionPlanner(Component):
             vehicle_poly_world = vehicle.to_object().polygon_parent() 
             if len(all_pedestrians) > 0:
                 #TODO: figure out a good way to check for collisions with pedestrians with the desired margins
-                pedestrian_x = a.pose.x
-                pedestrian_y = a.pose.y
-                pedestrian_w = a.dimensions[0]
-                pedestrian_h = a.dimensions[1]
-
-                dist_p2v_longi = np.abs(curr_x - pedestrian_x) # The distance between pedestrian and vehicle - longitudinal direction.
-                dist_p2v_lateral = np.abs(curr_y - pedestrian_y) # The distance between pedestrian and vehicle - lateral direction.
-
-                p_boundary_left = pedestrian_y + pedestrian_w
-                p_boundary_right = pedestrian_y - pedestrian_w
-                p_boundary_top = pedestrian_x + pedestrian_h
-                p_boundary_bot = pedestrian_x - pedestrian_h
-
-                v_boundary_left = curr_y + curr_w
-                v_boundary_right = curr_y - curr_w
-                v_boundary_top = curr_x + curr_h
-                v_boundary_bot = curr_x - curr_h
-
-                if (p_boundary_left > v_boundary_right or 
-                    p_boundary_right < v_boundary_left or 
-                    p_boundary_bot < v_boundary_top or 
-                    p_boundary_top > v_boundary_bot):
-
-                    print ("Collision !!")
-
+                ped_dist_list = []
+                for ped in all_pedestrians:
+                    ped_dist_list.append(np.hypot(vehicle.pose.x - ped.pose.x, vehicle.pose.y - ped.pose.y))
+                closest_ped = all_pedestrians[np.argmin(ped_dist_list)]
+                
+                V_x = vehicle.pose.x
+                V_y = vehicle.pose.y
+                V_l = 4.0
+                V_w = 1.6
+                P_x = closest_ped.pose.x
+                P_y = closest_ped.pose.y
+                P_l = closest_ped.dimensions[0]
+                P_w = closest_ped.dimensions[1]
+                margin_x = 3.0
+                margin_y = 1.0
+                ld_braking = (vehicle.v)**2 / (2 * self.deceleration) # self.desired_speed
+                
+                delta_x = abs(P_x - V_x) - 0.5*P_l - 0.5*V_l
+                delta_y = abs(P_y - V_y) - 0.5*P_w - 0.5*V_w
+                if (delta_x < margin_x + ld_braking) and (delta_y < margin_y):
+                    print('Collision warning!')
+                    max_progress = max(progress - collision_check_resolution, 0.0)
+                    break
+                                
                 if any([collisions.polygon_intersects_polygon_2d(vehicle_poly_world,a.polygon_parent()) for a in all_pedestrians]):
                     print("Predicted collision with pedestrian at",progress)
                     print("Vehicle position",xy[0],xy[1])
@@ -133,11 +124,10 @@ class PedestrianAvoidanceMotionPlanner(Component):
                     #prevent
                     max_progress = max(progress - collision_check_resolution,0.0)
                     break
-
             progress += collision_check_resolution
         if max_progress is not None:           
             #allow enough room to brake from the current velocity
-            braking_distance = 0.5*(curr_v)**2/self.deceleration * 0.96 
+            braking_distance = 0.5*(curr_v)**2/self.deceleration# * 0.96 
             max_progress = max(max_progress,braking_distance)
             route_with_lookahead = route_with_lookahead.trim(progress_start,max_progress)
         else:
@@ -145,16 +135,21 @@ class PedestrianAvoidanceMotionPlanner(Component):
         print("Progress",route_with_lookahead.domain()[1])
 
         #choose whether to accelerate, brake, or keep at current velocity
-        # if max_progress > 0:
-        #     traj = longitudinal_plan(route_with_lookahead, self.acceleration, self.deceleration, self.desired_speed, curr_v)
-        # else:
-        #     traj = longitudinal_brake(route_with_lookahead, self.deceleration, curr_v)
-        # print("Stopped distance",traj.length())
-
-        if max_progress > 0 and dist_p2v_longi > look_ahead_dist and dist_p2v_lateral > margin_lateral:
+        if max_progress > 0:
             traj = longitudinal_plan(route_with_lookahead, self.acceleration, self.deceleration, self.desired_speed, curr_v)
         else:
             traj = longitudinal_brake(route_with_lookahead, self.deceleration, curr_v)
         print("Stopped distance",traj.length())
-
+        
+        # should_brake = False
+        # if ():
+        #     should_brake = True
+        # should_accelerate = (not should_brake and curr_v < self.desired_speed)
+        # if should_accelerate:
+        #     traj = longitudinal_plan(route_with_lookahead, self.acceleration, self.deceleration, self.desired_speed, curr_v)
+        # elif should_brake:
+        #     traj = longitudinal_brake(route_with_lookahead, self.deceleration, curr_v)
+        # else:
+        #     traj = longitudinal_plan(route_with_lookahead, 0.0, self.deceleration, self.desired_speed, curr_v)
+        
         return traj 
