e3ca8902e7be1a16198d4e611da027828822f5c8
ChuLinHuang <ades81394@gmail.com>
2024-03-25 12:02:59 -0500

    add hw3 files

--- GEMstack/onboard/perception/object_tracking.py
+++ GEMstack/onboard/perception/object_tracking.py
@@ -7,57 +7,61 @@ from ultralytics.utils.plotting import Annotator, colors
 
 from collections import defaultdict
 
-track_history = defaultdict(lambda: [])
-model = YOLO("yolov8n.pt")
-names = model.model.names
+def tracking():
 
-video_path = "/home/jimmy/catkin_ws/src/GEMstack/data/testing_video.mp4"
-cap = cv2.VideoCapture(video_path)
-assert cap.isOpened(), "Error reading video file"
+    track_history = defaultdict(lambda: [])
+    model = YOLO("yolov8n.pt")
+    names = model.model.names
 
-w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+    # video_path = "/home/jimmy/catkin_ws/src/GEMstack/data/testing_video.mp4"
+    # cap = cv2.VideoCapture(video_path)
+    cap = cv2.VideoCapture(0) # default camera
+    assert cap.isOpened(), "Error reading video file"
 
-result = cv2.VideoWriter("object_tracking.avi",
-                       cv2.VideoWriter_fourcc(*'mp4v'),
-                       fps,
-                       (w, h))
+    w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
 
-while cap.isOpened():
-    success, frame = cap.read()
-    if success:
-        results = model.track(frame, persist=True, verbose=False)
-        boxes = results[0].boxes.xyxy.cpu()
+    result = cv2.VideoWriter("object_tracking.avi",
+                        cv2.VideoWriter_fourcc(*'mp4v'),
+                        fps,
+                        (w, h))
 
-        if results[0].boxes.id is not None:
+    # while cap.isOpened():
+    while True:
+        success, frame = cap.read()
+        if success:
+            results = model.track(frame, persist=True, verbose=False)
+            boxes = results[0].boxes.xyxy.cpu()
 
-            # Extract prediction results
-            clss = results[0].boxes.cls.cpu().tolist()
-            track_ids = results[0].boxes.id.int().cpu().tolist()
-            confs = results[0].boxes.conf.float().cpu().tolist()
+            if results[0].boxes.id is not None:
 
-            # Annotator Init
-            annotator = Annotator(frame, line_width=2)
+                # Extract prediction results
+                clss = results[0].boxes.cls.cpu().tolist()
+                track_ids = results[0].boxes.id.int().cpu().tolist()
+                confs = results[0].boxes.conf.float().cpu().tolist()
 
-            for box, cls, track_id in zip(boxes, clss, track_ids):
-                annotator.box_label(box, color=colors(int(cls), True), label=names[int(cls)])
+                # Annotator Init
+                annotator = Annotator(frame, line_width=2)
 
-                # Store tracking history
-                track = track_history[track_id]
-                track.append((int((box[0] + box[2]) / 2), int((box[1] + box[3]) / 2)))
-                if len(track) > 30:
-                    track.pop(0)
+                for box, cls, track_id in zip(boxes, clss, track_ids):
+                    annotator.box_label(box, color=colors(int(cls), True), label=names[int(cls)])
 
-                # Plot tracks
-                points = np.array(track, dtype=np.int32).reshape((-1, 1, 2))
-                cv2.circle(frame, (track[-1]), 7, colors(int(cls), True), -1)
-                cv2.polylines(frame, [points], isClosed=False, color=colors(int(cls), True), thickness=2)
+                    # Store tracking history
+                    track = track_history[track_id]
+                    track.append((int((box[0] + box[2]) / 2), int((box[1] + box[3]) / 2)))
+                    if len(track) > 30:
+                        track.pop(0)
 
-        result.write(frame)
-        if cv2.waitKey(1) & 0xFF == ord("q"):
+                    # Plot tracks
+                    points = np.array(track, dtype=np.int32).reshape((-1, 1, 2))
+                    cv2.circle(frame, (track[-1]), 7, colors(int(cls), True), -1)
+                    cv2.polylines(frame, [points], isClosed=False, color=colors(int(cls), True), thickness=2)
+
+            result.write(frame)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+        else:
             break
-    else:
-        break
 
-result.release()
-cap.release()
-cv2.destroyAllWindows()
+    result.release()
+    cap.release()
+    cv2.destroyAllWindows()
