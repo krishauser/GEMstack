0806eda549f089f270fd3e4ad08c60315c85b61d
hjh371 <2517948092@qq.com>
2024-03-30 14:06:55 -0500

    add heuristic

--- GEMstack/onboard/planning/route_planning.py
+++ GEMstack/onboard/planning/route_planning.py
@@ -4,12 +4,14 @@ from ...utils import serialization
 from ...state import AllState,VehicleState,Route,ObjectFrameEnum,Roadmap,Roadgraph
 from ...mathutils import collisions
 from ...mathutils.transforms import normalize_vector
+from .reeds_shepp import path_length, get_optimal_path, rad2deg
 import os
 import copy
 from time import time
 from dataclasses import replace
 from queue import PriorityQueue
 import numpy as np
+
 
 class StaticRoutePlanner(Component):
     """Reads a route from disk and returns it as the desired route."""
@@ -47,8 +49,8 @@ class StaticRoutePlanner(Component):
         return self.route
     
 class Node:
-    def __init__(self,s,cost,heuristic=0.,parent=None):
-        self.s = s
+    def __init__(self,state,cost,heuristic=0.,parent=None):
+        self.state = state
         self.cost = cost
         self.heuristic = heuristic
         self.parent = parent
@@ -67,45 +69,68 @@ class SearchNavigationRoutePlanner(Component):
         self.start = start
         self.start = [0.,0.,0.,0.,0.]
         self.end = end
-        x_bound = [min(self.start[0], self.end[0])-2, max(self.start[0], self.end[0])+8]
+        x_bound = [min(self.start[0], self.end[0])-2, max(self.start[0], self.end[0])+1]
         y_bound = [min(self.start[1], self.end[1])-2, max(self.start[1], self.end[1])+3]
         theta_bound = [0, 2*np.pi]
 
-        resolution = 1.
-        self.s_bound = (int((x_bound[1]-x_bound[0])/resolution), int((y_bound[1]-y_bound[0])/resolution), 8)
+        resolution = 0.5
+        self.s_bound = (int((x_bound[1]-x_bound[0])/resolution), int((y_bound[1]-y_bound[0])/resolution), 16)
         self.s2state = lambda s: [x_bound[0] + s[0]*(x_bound[1]-x_bound[0])/self.s_bound[0], \
                                   y_bound[0] + s[1]*(y_bound[1]-y_bound[0])/self.s_bound[1], \
-                                  theta_bound[0] + s[2]*(theta_bound[1]-theta_bound[0])/self.s_bound[2]]
+                                  theta_bound[0] + s[2]*(theta_bound[1]-theta_bound[0])/self.s_bound[2], \
+                                  s[3]]
         
         self.state2s = lambda state: [round((state[0]-x_bound[0])/(x_bound[1]-x_bound[0])*self.s_bound[0]), \
                                       round((state[1]-y_bound[0])/(y_bound[1]-y_bound[0])*self.s_bound[1]), \
-                                      round((state[2]-theta_bound[0])/(theta_bound[1]-theta_bound[0])*self.s_bound[2])%8]
+                                      round((state[2]-theta_bound[0])/(theta_bound[1]-theta_bound[0])*self.s_bound[2])%self.s_bound[2], \
+                                      state[3]]
         
-        def expand(s):
-            next_s = []
-            for k in range(-1,2):
-                # for f in [-1,1]: # -1 for backward, 1 for forward
-                for f in [1]:
-                    theta = (s[2] + k)*np.pi/4
-                    i = s[0] + f*round(np.cos(theta))
-                    j = s[1] + f*round(np.sin(theta))
-                    if i >= 0 and i < self.s_bound[0] and j >= 0 and j < self.s_bound[1]:
-                        next_s.append([i,j,(s[2]+k)%8])
-            return next_s
+        v = 1.0
+        L = 1.75
+        steer_max = np.pi/3
+        turn_radius = L/np.tan(steer_max)
+        
+        def sim(state,steer,f,dt=0.5):
+            x, y, theta, _ = state
+            dtheta = v*np.tan(steer)/L
+            x = x + f*np.cos(theta)*v*dt
+            y = y + f*np.sin(theta)*v*dt
+            theta = theta + dtheta*dt
+            return [x,y,theta,f]
+        
+        def expand(state):
+            next_states = []
+            for steer in [-steer_max,0,steer_max]:
+                for f in [-1,1]: # -1 for backward, 1 for forward
+                    next_state = sim(state,steer,f)
+                    x, y, theta, _ = next_state
+                    if x < x_bound[0] or x > x_bound[1] or y < y_bound[0] or y > y_bound[1]:
+                        continue
+                    next_states.append(next_state)
+            return next_states
         self.expand = expand
 
-        def heuristic(s1,s2,agents=[]):
-            state1 = self.s2state(s1)
-            state2 = self.s2state(s2)
-            return np.linalg.norm(np.array(state1[:2])-np.array(state2[:2]))
+        def heuristic(state1,state2,agents=[]):
+            xs,ys,ths = state1[:3]
+            start = [xs/turn_radius,ys/turn_radius,ths]
+            xe,ye,the = state2[:3]
+            end = [xe/turn_radius,ye/turn_radius,the]
+            h = path_length(get_optimal_path(start,end))*turn_radius
+            return h*2
         self.heuristic = heuristic
 
-        def cost(s1,s2):
-            state1 = self.s2state(s1)
-            state2 = self.s2state(s2)
-            return np.linalg.norm(np.array(state1[:2])-np.array(state2[:2])) + \
-                   abs(state1[2]-state2[2])
+        def cost(state1,state2):
+            scale = 1.2 if state2[3] < 0 else 1.0
+            c = np.linalg.norm(np.array(state1[:2])-np.array(state2[:2]))
+            c = c*scale
+            c += 3 if state1[3]*state2[3] < 0 else 0
+            return c
         self.cost = cost
+
+        def distance(state1,state2):
+            return np.linalg.norm(np.array(state1[:3])-np.array(state2[:3])) + \
+                min(abs(state1[2]-state2[2])%(2*np.pi), 2*np.pi-abs(state1[2]-state2[2])%(2*np.pi))
+        self.distance = distance
 
         self.route = None
         self.LATERAL_DISTANCE_BUFFER = .5
@@ -142,10 +167,10 @@ class SearchNavigationRoutePlanner(Component):
         agents = [a.to_frame(ObjectFrameEnum.START, start_pose_abs=state.start_vehicle_pose) for a in agents.values()]
         # TODO: make this a real route
 
-        def check_constraints(s):
+        def check_constraints(state):
+            s = self.state2s(state)
             if s[0] < 0 or s[0] >= self.s_bound[0] or s[1] < 0 or s[1] >= self.s_bound[1]:
                 return False
-            state = self.s2state(s)
             vehicle_ = copy.deepcopy(vehicle)
             vehicle_.pose = replace(vehicle_.pose,x=state[0],y=state[1],yaw=state[2])
             vehicle_object = vehicle_.to_object()
@@ -159,8 +184,8 @@ class SearchNavigationRoutePlanner(Component):
             return True
         
         if True: # always replan
-            start = self.state2s(self.start)
-            end = self.state2s(self.end)
+            start = [*self.start[:3], 0]
+            end = [*self.end[:3], 0]
             root = Node(start,0,heuristic=self.heuristic(start,end))
 
             queue = PriorityQueue()
@@ -171,18 +196,19 @@ class SearchNavigationRoutePlanner(Component):
 
             while not queue.empty():
                 current = queue.get()
-                if self.cost(current.s,end) < 0.5:
+                if self.distance(current.state,end) < 1.0:
                     break
-                for s in self.expand(current.s):
-                    if tuple(s) not in visited and check_constraints(s):
+                for state in self.expand(current.state):
+                    s = self.state2s(state)
+                    if tuple(s) not in visited and check_constraints(state):
                         visited.add(tuple(s))
-                        cost = self.cost(current.s,s)
-                        child = Node(s,cost,heuristic=self.heuristic(s,end),parent=current)
+                        cost = self.cost(current.state,state) + current.cost
+                        child = Node(state,cost,heuristic=self.heuristic(state,end),parent=current)
                         queue.put(child)
             
             route = []
             while current is not None:
-                route.append(self.s2state(current.s)[:2])
+                route.append(current.state[:2])
                 current = current.parent
             route = route[::-1]
             self.route = Route(frame=ObjectFrameEnum.START,points=route)
