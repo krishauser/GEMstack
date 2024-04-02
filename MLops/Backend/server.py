ce9ac45397c731c5956b08a15e84c18006ecb2d7
congyoua <y.cong@mail.utoronto.ca>
2024-04-02 17:08:22 -0500

    update server for mongodb schema

--- MLops/Backend/server.py
+++ MLops/Backend/server.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, send_file, render_template
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import json
+import os
 
 app = Flask(__name__)
 
@@ -22,6 +23,8 @@ def load_config(file_path):
 
 try:
     config = load_config('config.json')
+    client = MongoClient(config['MONGO_URI'])
+    db = client[config['DATABASE_NAME']]
 except FileNotFoundError as e:
     print(f"Error: {e}")
     exit(1)
@@ -32,54 +35,50 @@ except ValueError as e:
 
 @app.route('/models/')
 def get_models():
-    models = db.models.find()
-    return jsonify([model for model in models])
+    models = db.Models.find({}, {'_id': 0, 'ID': 1, 'ModelName': 1, 'Path': 1, 'Description': 1})
+    return jsonify(list(models))
 
 
-@app.route('/models/<id>')
+@app.route('/models/<int:id>')
 def get_model(id):
-    # To be implemented
-    model_info = db.models.find_one({'_id': ObjectId(id)})
-    if model:
-        return jsonify(model_info)
+    model_info = db.Models.find_one({'ID': id}, {'_id': 0, 'ID': 1, 'ModelName': 1, 'Path': 1, 'Description': 1})
+    if model_info:
+        return jsonify(model_info )
     else:
         return jsonify({"error": "Model not found"}), 404
 
 
-@app.route('/models/retrieval/<id>')
+@app.route('/models/retrieval/<int:id>')
 def retrieve_model(id):
-    # To be implemented
-    model = db.models.find_one({'_id': ObjectId(id)})
-    if model and 'path' in model:
-        return send_file(model['path'], attachment_filename=id, as_attachment=True)
+    model = db.Models.find_one({'ID': id}, {'_id': 0, 'Path': 1})
+    if model and 'Path' in model:
+        return send_file(model['Path'], as_attachment=True)
     else:
-        return jsonify({"error": "Model not found or path missing"}), 404
+        return jsonify({"error": "Model not found or address missing"}), 404
 
 
 @app.route('/datasets/')
 def get_datasets():
-    datasets = db.datasets.find()
-    return jsonify([dataset for dataset in datasets])
+    datasets = db.Data.find({}, {'_id': 0, 'ID': 1, 'DataName': 1, 'Path': 1, 'Description': 1})
+    return jsonify(list(datasets))
 
 
-@app.route('/datasets/<id>')
+@app.route('/datasets/<int:id>')
 def get_dataset(id):
-    # To be implemented
-    dataset = db.datasets.find_one({'_id': ObjectId(id)})
+    dataset = db.Data.find_one({'ID': id}, {'_id': 0, 'ID': 1, 'DataName': 1, 'Path': 1, 'Description': 1})
     if dataset:
         return jsonify(dataset)
     else:
         return jsonify({"error": "Dataset not found"}), 404
 
 
-@app.route('/datasets/retrieval/<id>')
+@app.route('/datasets/retrieval/<int:id>')
 def retrieve_dataset(id):
-    # To be implemented
-    dataset = db.datasets.find_one({'_id': ObjectId(id)})
-    if dataset and 'path' in dataset:
-        return send_file(dataset['path'], attachment_filename=f"{id}.zip", as_attachment=True)
+    dataset = db.Data.find_one({'ID': id}, {'_id': 0, 'Path': 1})
+    if dataset and 'Path' in dataset:
+        return send_file(dataset['Path'], as_attachment=True)
     else:
-        return jsonify({"error": "Dataset not found or path missing"}), 404
+        return jsonify({"error": "Dataset not found or address missing"}), 404
 
 
 if __name__ == '__main__':
