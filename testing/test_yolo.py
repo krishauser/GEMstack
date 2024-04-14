b001db4f72586b37433d1a7ddc7b29579a047443
Jacky2 <jackyyeh511@gmail.com>
2024-04-14 07:37:35 -0500

    update test_yolo.py

--- testing/test_yolo.py
+++ testing/test_yolo.py
@@ -1,3 +1,8 @@
+""" 
+    Usage:
+        python test_yolo.py -t <target_task>
+"""
+
 import time
 import math
 import numpy as np
@@ -37,25 +42,10 @@ camera_image = None
 depth_image = None
 bridge = CvBridge()
 
-colors = []
-colors += [(i, 0, 255) for i in range(100, 256, 25)]
-colors += [(i, 255, 0) for i in range(100, 256, 25)]
-colors += [(0, i, 255) for i in range(100, 256, 25)]
-colors += [(255, i, 0) for i in range(100, 256, 25)]
-colors += [(255, 0, i) for i in range(100, 256, 25)]
-colors += [(0, 255, i) for i in range(100, 256, 25)]
-seed = 19
-random.seed(seed)
-random.shuffle(colors)
-
-id2str = {
-    0: "pedestrian",
-    2: "car",
-    11: "stop sign"
-}
-
 parser = argparse.ArgumentParser()
-parser.add_argument('--output_dir', '-o', type=str, default='save')
+parser.add_argument('--output_dir', '-o', type=str, default='output')
+parser.add_argument('--test_target', '-t', type=str, required=True, 
+                    choices=['yolo_ros', 'yolo_only'])
 args = parser.parse_args()
 
 OUTPUT_DIR = args.output_dir
@@ -86,33 +76,6 @@ def ros_PointCloud2_to_numpy(pc2_msg, want_rgb = False):
         return xyzrgb
     else:
         return np.array(list(gen),dtype=np.float32)[:,:3]
-    
-def lidar_to_image(point_cloud_lidar: np.ndarray, extrinsic : np.ndarray, intrinsic : np.ndarray):
-    
-    homo_point_cloud_lidar = np.hstack((point_cloud_lidar, np.ones((point_cloud_lidar.shape[0], 1)))) # (N, 4)
-    pointcloud_pixel = (intrinsic @ extrinsic @ (homo_point_cloud_lidar).T) # (3, N)
-    pointcloud_pixel = pointcloud_pixel.T # (N, 3)
-
-    # normalize
-    pointcloud_pixel[:, 0] /= pointcloud_pixel[:, 2] # normalize
-    pointcloud_pixel[:, 1] /= pointcloud_pixel[:, 2] # normalize
-    point_cloud_image =  pointcloud_pixel[:,:2] # (N, 2)
-    return point_cloud_image
-
-def lidar_to_vehicle(point_cloud_lidar: np.ndarray, T_lidar2_Gem: np.ndarray):
-    ones = np.ones((point_cloud_lidar.shape[0], 1))
-    pcd_homogeneous = np.hstack((point_cloud_lidar, ones)) # (N, 4)
-    pointcloud_trans = np.dot(T_lidar2_Gem, pcd_homogeneous.T) # (4, N)
-    pointcloud_trans = pointcloud_trans.T # (N, 4)
-    point_cloud_image_world = pointcloud_trans[:, :3] # (N, 3)
-    return point_cloud_image_world
-
-def filter_lidar_by_range(point_cloud, xrange: Tuple[float, float], yrange: Tuple[float, float]):
-    xmin, xmax = xrange
-    ymin, ymax = yrange
-    idxs = np.where((point_cloud[:, 0] > xmin) & (point_cloud[:, 0] < xmax) &
-                    (point_cloud[:, 1] > ymin) & (point_cloud[:, 1] < ymax) )
-    return point_cloud[idxs]
 
 class PedestrianDetector():
     """Detects and tracks pedestrians."""
@@ -158,8 +121,7 @@ class PedestrianDetector():
             print(e)
 
         self.zed_image = cv_image
-        cv2.imwrite("test.png", self.zed_image)
-        exit(1)
+        # cv2.imwrite("test.png", self.zed_image)
 
     def lidar_callback(self, point_cloud):
         # self.point_cloud = point_cloud
@@ -208,47 +170,39 @@ class PedestrianDetector():
         cv2.imshow('frame', vis)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             exit(1)
-        
-        
-        # pathlib.Path(OUTPUT_DIR).mkdir(parents=True, exist_ok=True)
-        
-        # color_fn = os.path.join(OUTPUT_DIR,'color{}.png'.format(self.index))
-        # lidar_fn = os.path.join(OUTPUT_DIR,'lidar{}.npz'.format(self.index))
-        
-        # print ('Output result:', color_fn)
-        # cv2.imwrite(color_fn, vis)
-        # np.savez(lidar_fn, vis_pc)
     
 def main():
-    # args = parser.parse_args()
-    # print ('======= Initial arguments =======')
-    # params = []
-    # for key, val in vars(args).items():
-    #     param = f"--{key} {val}"
-    #     print(f"{key} => {val}")
-    #     params.append(param)
+    args = parser.parse_args()
+    print ('======= Initial arguments =======')
+    params = []
+    for key, val in vars(args).items():
+        param = f"--{key} {val}"
+        print(f"{key} => {val}")
+        params.append(param)
 
-    # rospy.init_node('rgb_track_node', anonymous=True)
-    # rate = rospy.Rate(30)  # Hz
+    if args.test_target == 'yolo_ros':
+        rospy.init_node('rgb_track_node', anonymous=True)
+        rate = rospy.Rate(30)  # Hz
 
-    # ped = PedestrianDetector()
+        ped = PedestrianDetector()
 
-    # try:
-    #     print ('\nStart navigation...')
-    #     while not rospy.is_shutdown():
-    #         rate.sleep()  # Wait a while before trying to get a new waypoints
-    #         ped.detect_agents()
-    # except rospy.ROSInterruptException:
-    #     pass
+        try:
+            print ('\nStart detection...')
+            while not rospy.is_shutdown():
+                rate.sleep()  # Wait a while before trying to get a new waypoints
+                ped.detect_agents()
+        except rospy.ROSInterruptException:
+            pass
+    
+    elif args.test_target == 'yolo_only':
+        img = cv2.imread('test.png')
+        yolo_path = os.path.join(abs_path, '../GEMstack/knowledge/detection/yolov8n.pt')
+        detector = YOLO(yolo_path)
 
-    img = cv2.imread('test.png')
-    yolo_path = os.path.join(abs_path, '../GEMstack/knowledge/detection/yolov8n.pt')
-    detector = YOLO(yolo_path)
-
-    for i in range(10): 
-        t1 = time.time()
-        detection_result = detector(img,verbose=False)
-        print (f'{i} detection time:', time.time() - t1)
+        for i in range(10): 
+            t1 = time.time()
+            detection_result = detector(img,verbose=False)
+            print (f'{i} detection time:', time.time() - t1)
 
 
 if __name__ == '__main__':
