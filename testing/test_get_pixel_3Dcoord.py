307cebc6a1b082e643c0674598a52aaaf2205756
Jacky2 <jackyyeh511@gmail.com>
2024-04-18 15:22:39 -0500

    update test_get_pixel_3Dcoord.py

--- testing/test_get_pixel_3Dcoord.py
+++ testing/test_get_pixel_3Dcoord.py
@@ -12,24 +12,10 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--output_dir', '-o', type=str, default='save')
 parser.add_argument('--src_dir', '-s', type=str, default='./data/gt')
-parser.add_argument('--data_idx', '-i', type=int, default=1)
+parser.add_argument('--data_idx', '-i', type=int, default=3)
 
 args = parser.parse_args()
-
 OUTPUT_DIR = args.output_dir
-
-from sklearn.cluster import DBSCAN
-
-colors = []
-colors += [(i, 0, 255) for i in range(100, 256, 25)]
-colors += [(i, 255, 0) for i in range(100, 256, 25)]
-colors += [(0, i, 255) for i in range(100, 256, 25)]
-colors += [(255, i, 0) for i in range(100, 256, 25)]
-colors += [(255, 0, i) for i in range(100, 256, 25)]
-colors += [(0, 255, i) for i in range(100, 256, 25)]
-seed = 19
-random.seed(seed)
-random.shuffle(colors)
 
 def filter_lidar_by_range(point_cloud, xrange: Tuple[float, float], yrange: Tuple[float, float]):
     xmin, xmax = xrange
@@ -50,57 +36,81 @@ def lidar_to_image(point_cloud_lidar: np.ndarray, extrinsic : np.ndarray, intrin
     point_cloud_image =  pointcloud_pixel[:,:2] # (N, 2)
     return point_cloud_image
 
-class TestHelper:
-    def __init__(self, point_cloud, zed_image, depth):
-        self.point_cloud = point_cloud
-        self.zed_image = zed_image
-        self.depth = depth
+def lidar_to_vehicle(point_cloud_lidar: np.ndarray, T_lidar2_Gem: np.ndarray):
+    ones = np.ones((point_cloud_lidar.shape[0], 1))
+    pcd_homogeneous = np.hstack((point_cloud_lidar, ones)) # (N, 4)
+    pointcloud_trans = np.dot(T_lidar2_Gem, pcd_homogeneous.T) # (4, N)
+    pointcloud_trans = pointcloud_trans.T # (N, 4)
+    point_cloud_image_world = pointcloud_trans[:, :3] # (N, 3)
+    return point_cloud_image_world
+
+
+coord_3d_map = None
+def mouse_callback(event, x, y, flags, param):
+    global coord_3d_map
+    if event == cv2.EVENT_MOUSEMOVE:
+        # Display pixel coordinates on the console
+        print(f"Pixel coordinates: x={x}, y={y}, 3D coord: {coord_3d_map[y][x]}")
+
+
+extrinsic = np.loadtxt("GEMstack/knowledge/calibration/gem_e4_lidar2oak.txt")    
+intrinsic = np.loadtxt("GEMstack/knowledge/calibration/gem_e4_intrinsic.txt")
+intrinsic = np.concatenate([intrinsic, np.zeros((3, 1))], axis=1)
+
+T_lidar2_Gem = np.loadtxt("GEMstack/knowledge/calibration/gem_e4_lidar2vehicle.txt")
+
+# Hardcode the roi area for agents
+xrange = (0, 10)
+yrange = (-10, 10)
+
+def get3DCoordByPixels(image, point_cloud):
+    global coord_3d_map
+    print ('\nStart testing...')
+    filtered_point_cloud = filter_lidar_by_range(point_cloud, xrange,yrange)
+    
+    point_cloud_3D = lidar_to_vehicle(filtered_point_cloud, T_lidar2_Gem)
+    
+    point_cloud_image = lidar_to_image(filtered_point_cloud, 
+                                        extrinsic, 
+                                        intrinsic)
+    
+    # Keep only the lidar points whose projected coordinates lie  within the boundary of images
+    height, width = image.shape[:2]
+    idxs = np.where((point_cloud_image[:, 0] > 0) & (point_cloud_image[:, 0] < width) &
+                    (point_cloud_image[:, 1] > 0) & (point_cloud_image[:, 1] < height) )[0]
+
+    print ('# of point_cloud_image:', len(point_cloud_image)) # 131072
+    print ('# of filtered_point_cloud:', len(idxs))
+    
+    # initialize coord_3d_map
+    coord_3d_map = np.zeros(shape=(height, width, 3), dtype=point_cloud_3D.dtype)
+    for idx in idxs:
+        u, v = int(point_cloud_image[idx][0]), int(point_cloud_image[idx][1])
         
-        self.extrinsic = np.loadtxt("GEMstack/knowledge/calibration/gem_e4_lidar2oak.txt")    
-        intrinsic = np.loadtxt("GEMstack/knowledge/calibration/gem_e4_intrinsic.txt")
-        self.intrinsic = np.concatenate([intrinsic, np.zeros((3, 1))], axis=1)
-
-        T_lidar2_Gem = np.loadtxt("GEMstack/knowledge/calibration/gem_e4_lidar2vehicle.txt")
-        self.T_lidar2_Gem = np.asarray(T_lidar2_Gem)
-
-        # Hardcode the roi area for agents
-        self.xrange = (0, 100)
-        self.yrange = (-100, 100)
-
-    def run(self):
-        print ('\nStart testing...')
-        filtered_point_cloud = filter_lidar_by_range(self.point_cloud, 
-                                                     self.xrange,
-                                                     self.yrange)
-        
-        point_cloud_image = lidar_to_image(filtered_point_cloud, 
-                                           self.extrinsic, 
-                                           self.intrinsic)
-        vis = self.zed_image.copy()
-        for proj_pt in point_cloud_image:
-            color = (0, 255, 0) # green
-            radius = 1
-            center = int(proj_pt[0]), int(proj_pt[1])
-            vis = cv2.circle(vis, center, radius, color, cv2.FILLED)
-        
-        pathlib.Path(OUTPUT_DIR).mkdir(parents=True, exist_ok=True)
-        output_path = os.path.join(OUTPUT_DIR, f'lidar_to_image_{args.data_idx}.png')
-        print ('Output lidar_to_image result:', output_path)
-        cv2.imwrite(output_path, vis)
-
+        coord_3d_map[v][u][:] = point_cloud_3D[idx]
+    
+    return coord_3d_map
 
 if __name__=='__main__':
     # load data
     lidar_fn = os.path.join(args.src_dir, f'lidar{args.data_idx}.npz')
     image_fn = os.path.join(args.src_dir, f'color{args.data_idx}.png')
-    depth_fn = os.path.join(args.src_dir, f'depth{args.data_idx}.tif')
+    # depth_fn = os.path.join(args.src_dir, f'depth{args.data_idx}.tif')
     
     point_cloud = np.load(lidar_fn)['arr_0']
     image = cv2.imread(image_fn)
-    depth = cv2.imread(depth_fn)
+    coord_3d_map = get3DCoordByPixels(image, point_cloud)
     
-    test_helper = TestHelper(point_cloud, image, depth)
+    # check pixel 3D coords
+    cv2.namedWindow('Image')
+    cv2.setMouseCallback('Image', mouse_callback)
+    while True:
+        cv2.imshow('Image', image)
+        
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
     
-    test_helper.run()
+    cv2.destroyAllWindows()
     print ('\nDone!')
     
