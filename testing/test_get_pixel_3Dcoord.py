b7e773ef694430aa0c4c1370e282fcdac1d524aa
Jacky2 <jackyyeh511@gmail.com>
2024-04-19 09:50:24 -0500

    implement interpolation in test_get_pixel_3Dcoord.py

--- testing/test_get_pixel_3Dcoord.py
+++ testing/test_get_pixel_3Dcoord.py
@@ -10,13 +10,79 @@ import pathlib
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--output_dir', '-o', type=str, default='save')
 parser.add_argument('--src_dir', '-s', type=str, default='./data/gt')
+parser.add_argument('--test_target', '-t', type=str, default='interpolation', 
+                    choices=['interpolation', 'test_conv'])
 parser.add_argument('--data_idx', '-i', type=int, default=3)
+parser.add_argument('--kernel_size', '-k', type=int, default=5)
 
 args = parser.parse_args()
-OUTPUT_DIR = args.output_dir
 
+def test_conv_2D():
+    # Define a sample image and kernel
+    image = np.array([
+        [10, 0, 0],
+        [0, 20, 0],
+        [0, 0, 30]
+    ], dtype=np.float32)
+
+    # Do convolution
+    kernel = np.array([
+                 [1, 1],
+                 [1, 1]
+             ], dtype=np.float32)
+    kernel = cv2.flip(kernel, flipCode=-1)
+    result = cv2.filter2D(image, -1, kernel, borderType=cv2.BORDER_CONSTANT)
+
+    # Derive num_elements matrix
+    has_value = image > 0
+    has_value = has_value.astype(np.uint8)
+    num_elements_mat = cv2.filter2D(has_value, -1, kernel, borderType=cv2.BORDER_CONSTANT)
+    
+    # divide by num of elements correspond to that particular pixel
+    num_elements_mat[num_elements_mat == 0] = 1 # avoid divide by zero
+    interpolation_result = result / num_elements_mat
+
+    print("Image:")
+    print(image)
+    print("\nKernel:")
+    print(kernel)
+    print("\nResult:")
+    print(result)
+    print("\nnum_elements_mat:")
+    print(num_elements_mat)
+    print("\n Interpolation Result:")
+    print(interpolation_result)
+
+def interpolate(image):
+    def interpolate_single_channel(image, kernel):
+        kernel = cv2.flip(kernel, flipCode=-1)
+        result = cv2.filter2D(image, -1, kernel, borderType=cv2.BORDER_CONSTANT)
+
+        # Derive num_elements matrix
+        has_value = image > 0
+        has_value = has_value.astype(np.uint8)
+        num_elements_mat = cv2.filter2D(has_value, -1, kernel, borderType=cv2.BORDER_CONSTANT)
+    
+        # divide by num of elements correspond to that particular pixel
+        num_elements_mat[num_elements_mat == 0] = 1 # avoid divide by zero
+        final_result = result / num_elements_mat # divide element-wise
+        return final_result
+    
+    # dilate and interpolation
+    channels = []
+    num_channels = coord_3d_map.shape[-1]
+    for i in range(num_channels):
+        channel = coord_3d_map[:, :, i]
+        kernel = np.ones((args.kernel_size, args.kernel_size), dtype=np.float32)
+        result = interpolate_single_channel(channel, kernel)
+        channels.append(result)
+        print ('result.shape:', result.shape)
+
+    interpolate_3D_map = cv2.merge(channels)
+    return interpolate_3D_map
+        
+        
 def filter_lidar_by_range(point_cloud, xrange: Tuple[float, float], yrange: Tuple[float, float]):
     xmin, xmax = xrange
     ymin, ymax = yrange
@@ -50,7 +116,8 @@ def mouse_callback(event, x, y, flags, param):
     global coord_3d_map
     if event == cv2.EVENT_MOUSEMOVE:
         # Display pixel coordinates on the console
-        print(f"Pixel coordinates: x={x}, y={y}, 3D coord: {coord_3d_map[y][x]}")
+        x_3d, y_3d, z_3d = coord_3d_map[y][x]
+        print(f"Pixel coordinates: x={x}, y={y} | 3D xyz coord: x={x_3d:.2f}, y={y_3d:.2f}, z={z_3d:.2f} (in meters)")
 
 
 extrinsic = np.loadtxt("GEMstack/knowledge/calibration/gem_e4_lidar2oak.txt")    
@@ -85,32 +152,41 @@ def get3DCoordByPixels(image, point_cloud):
     # initialize coord_3d_map
     coord_3d_map = np.zeros(shape=(height, width, 3), dtype=point_cloud_3D.dtype)
     for idx in idxs:
-        u, v = int(point_cloud_image[idx][0]), int(point_cloud_image[idx][1])
-        
+        u, v = int(point_cloud_image[idx][0]), int(point_cloud_image[idx][1])    
         coord_3d_map[v][u][:] = point_cloud_3D[idx]
     
-    return coord_3d_map
+    # interpolate
+    interpolate_3D_map = interpolate(coord_3d_map)
+    print ('interpolate_3D_map shape:', interpolate_3D_map.shape)
+    
+    return interpolate_3D_map
 
 if __name__=='__main__':
     # load data
     lidar_fn = os.path.join(args.src_dir, f'lidar{args.data_idx}.npz')
     image_fn = os.path.join(args.src_dir, f'color{args.data_idx}.png')
-    # depth_fn = os.path.join(args.src_dir, f'depth{args.data_idx}.tif')
     
     point_cloud = np.load(lidar_fn)['arr_0']
     image = cv2.imread(image_fn)
-    coord_3d_map = get3DCoordByPixels(image, point_cloud)
     
-    # check pixel 3D coords
-    cv2.namedWindow('Image')
-    cv2.setMouseCallback('Image', mouse_callback)
-    while True:
-        cv2.imshow('Image', image)
+    if args.test_target == 'test_conv':
+        ### verify the correctness of opencv filter2D ###
+        test_conv_2D()
         
-        # Break the loop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    
-    cv2.destroyAllWindows()
+    elif args.test_target == 'interpolation':
+        coord_3d_map = get3DCoordByPixels(image, point_cloud)
+        
+        # check pixel 3D coords in interactive mode
+        cv2.namedWindow('Image')
+        cv2.setMouseCallback('Image', mouse_callback)
+        while True:
+            cv2.imshow('Image', image)
+            
+            # Break the loop if 'q' is pressed
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        
+        cv2.destroyAllWindows()
+        
     print ('\nDone!')
-    
+        
