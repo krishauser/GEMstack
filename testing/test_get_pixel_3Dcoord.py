d921ba42f253f44add5188a2ef70f77a7d0b74c7
Jacky2 <jackyyeh511@gmail.com>
2024-04-21 14:34:43 -0500

    support rosbag for get_pixel_3Dcoord

--- testing/test_get_pixel_3Dcoord.py
+++ testing/test_get_pixel_3Dcoord.py
@@ -18,13 +18,66 @@ parser.add_argument('--kernel_size', '-k', type=int, default=5)
 args = parser.parse_args()
 
 coord_3d_map = None
+mouse_x = None
+mouse_y = None
+
 def mouse_callback(event, x, y, flags, param):
     global coord_3d_map
-    if event == cv2.EVENT_MOUSEMOVE:
-        # Display pixel coordinates on the console
-        x_3d, y_3d, z_3d = coord_3d_map[y][x]
-        print(
-            f"Pixel coordinates: x={x}, y={y} | 3D xyz coord: x={x_3d:.2f}, y={y_3d:.2f}, z={z_3d:.2f} (in meters)")
+    global mouse_x
+    global mouse_y
+    mouse_x = x
+    mouse_y = y
+    # if event == cv2.EVENT_MOUSEMOVE:
+    # Display pixel coordinates on the console
+    x_3d, y_3d, z_3d = coord_3d_map[y][x]
+    
+    print(
+        f"Pixel coordinates: x={x}, y={y} | 3D xyz coord: x={x_3d:.2f}, y={y_3d:.2f}, z={z_3d:.2f} (in meters)")
+
+
+if args.test_target == 'rosbag':
+    import sensor_msgs.point_cloud2 as pc2
+    from cv_bridge import CvBridge
+    import rospy
+    from sensor_msgs.msg import Image, PointCloud2
+
+    class TestNode:
+        def __init__(self, coord_3d_handler):
+            self.coord_3d_handler = coord_3d_handler
+            self.bridge = CvBridge()
+            rospy.Subscriber('/ouster/points', PointCloud2,
+                             self.lidar_callback)
+            rospy.Subscriber('/oak/rgb/image_raw', Image, self.camera_callback)
+            self.image = None
+            self.point_cloud = None
+
+        def camera_callback(self, image: Image):
+            self.image = image
+
+        def lidar_callback(self, point_cloud):
+            self.point_cloud = point_cloud
+
+        def can_run(self):
+            if self.image is None or self.point_cloud is None:
+                return False
+            return True
+
+        def run(self):
+            # convert data type
+            cv_image = self.bridge.imgmsg_to_cv2(self.image, "bgr8")
+            numpy_point_cloud = ros_PointCloud2_to_numpy(
+                self.point_cloud, want_rgb=False)
+
+            # get3DCoord
+            coord_3d_map = self.coord_3d_handler.get3DCoord(
+                cv_image, numpy_point_cloud)
+
+            # cv2.imshow('frame', cv_image)
+            # cv2.setMouseCallback('Image', self.mouse_callback)
+            # if cv2.waitKey(1) & 0xFF == ord("q"):
+            #     exit(1)
+
+            return coord_3d_map, cv_image.copy()
 
 
 def test_conv_2D():
@@ -65,6 +118,36 @@ def test_conv_2D():
     print(interpolation_result)
 
 
+def ros_PointCloud2_to_numpy(pc2_msg, want_rgb=False):
+    if pc2 is None:
+        raise ImportError("ROS is not installed")
+    # gen = pc2.read_points(pc2_msg, skip_nans=True)
+    gen = pc2.read_points(pc2_msg, skip_nans=True, field_names=['x', 'y', 'z'])
+
+    if want_rgb:
+        xyzpack = np.array(list(gen), dtype=np.float32)
+        if xyzpack.shape[1] != 4:
+            raise ValueError(
+                "PointCloud2 does not have points with color data.")
+        xyzrgb = np.empty((xyzpack.shape[0], 6))
+        xyzrgb[:, :3] = xyzpack[:, :3]
+        for i, x in enumerate(xyzpack):
+            rgb = x[3]
+            # cast float32 to int so that bitwise operations are possible
+            s = struct.pack('>f', rgb)
+            i = struct.unpack('>l', s)[0]
+            # you can get back the float value by the inverse operations
+            pack = ctypes.c_uint32(i).value
+            r = (pack & 0x00FF0000) >> 16
+            g = (pack & 0x0000FF00) >> 8
+            b = (pack & 0x000000FF)
+            # r,g,b values in the 0-255 range
+            xyzrgb[i, 3:] = (r, g, b)
+        return xyzrgb
+    else:
+        return np.array(list(gen), dtype=np.float32)[:, :3]
+
+
 class PixelWise3DLidarCoordHandler:
     """  """
 
@@ -73,16 +156,18 @@ class PixelWise3DLidarCoordHandler:
             "GEMstack/knowledge/calibration/gem_e4_lidar2oak.txt")
         self.intrinsic = np.loadtxt(
             "GEMstack/knowledge/calibration/gem_e4_intrinsic.txt")
-        self.intrinsic = np.concatenate([self.intrinsic, np.zeros((3, 1))], axis=1)
+        self.intrinsic = np.concatenate(
+            [self.intrinsic, np.zeros((3, 1))], axis=1)
 
         self.T_lidar2_Gem = np.loadtxt(
             "GEMstack/knowledge/calibration/gem_e4_lidar2vehicle.txt")
 
         self.kernel_size = kernel_size
-        
+
         # Hardcode the roi area for agents
         self.xrange = (0, 10)
-        self.yrange = (-10, 10)
+        self.yrange = (-5, 5)
+        self.zrange = (-2.5, 0)
 
     def interpolate(self, coord_3d_map):
         def interpolate_single_channel(image, kernel):
@@ -111,16 +196,17 @@ class PixelWise3DLidarCoordHandler:
                 (self.kernel_size, self.kernel_size), dtype=np.float32)
             result = interpolate_single_channel(channel, kernel)
             channels.append(result)
-            print('result.shape:', result.shape)
 
         interpolate_3D_map = cv2.merge(channels)
         return interpolate_3D_map
 
-    def filter_lidar_by_range(self, point_cloud, xrange: Tuple[float, float], yrange: Tuple[float, float]):
+    def filter_lidar_by_range(self, point_cloud, xrange: Tuple[float, float], yrange: Tuple[float, float], zrange: Tuple[float, float]):
         xmin, xmax = xrange
         ymin, ymax = yrange
+        zmin, zmax = zrange
         idxs = np.where((point_cloud[:, 0] > xmin) & (point_cloud[:, 0] < xmax) &
-                        (point_cloud[:, 1] > ymin) & (point_cloud[:, 1] < ymax))
+                        (point_cloud[:, 1] > ymin) & (point_cloud[:, 1] < ymax) &
+                        (point_cloud[:, 2] > zmin) & (point_cloud[:, 2] < zmax))
         return point_cloud[idxs]
 
     def lidar_to_image(self, point_cloud_lidar: np.ndarray, extrinsic: np.ndarray, intrinsic: np.ndarray):
@@ -146,9 +232,8 @@ class PixelWise3DLidarCoordHandler:
         return point_cloud_image_world
 
     def get3DCoord(self, image, point_cloud):
-        print('\nStart testing...')
         filtered_point_cloud = self.filter_lidar_by_range(
-            point_cloud, self.xrange, self.yrange)
+            point_cloud, self.xrange, self.yrange, self.zrange)
 
         point_cloud_3D = self.lidar_to_vehicle(
             filtered_point_cloud, self.T_lidar2_Gem)
@@ -180,18 +265,22 @@ class PixelWise3DLidarCoordHandler:
 
 
 if __name__ == '__main__':
-    # load data
-    lidar_fn = os.path.join(args.src_dir, f'lidar{args.data_idx}.npz')
-    image_fn = os.path.join(args.src_dir, f'color{args.data_idx}.png')
-
-    point_cloud = np.load(lidar_fn)['arr_0']
-    image = cv2.imread(image_fn)
 
     if args.test_target == 'test_conv':
         ### verify the correctness of opencv filter2D ###
         test_conv_2D()
 
     elif args.test_target == 'frame':
+        # load data
+        lidar_fn = os.path.join(args.src_dir, f'lidar{args.data_idx}.npz')
+        image_fn = os.path.join(args.src_dir, f'color{args.data_idx}.png')
+
+        point_cloud = np.load(lidar_fn)['arr_0']
+        image = cv2.imread(image_fn)
+
+        print('\nStart testing...')
+
+        # init handler
         handler = PixelWise3DLidarCoordHandler(args.kernel_size)
         coord_3d_map = handler.get3DCoord(image, point_cloud)
 
@@ -208,6 +297,32 @@ if __name__ == '__main__':
         cv2.destroyAllWindows()
 
     elif args.test_target == 'rosbag':
-        pass
-        
+        rospy.init_node('test_node')
+        rate = rospy.Rate(30)  # Hz
+
+        # init class object
+        handler = PixelWise3DLidarCoordHandler(args.kernel_size)
+        test_node = TestNode(handler)
+
+        try:
+            print('\nStart testing...')
+            cv2.namedWindow('Image')
+            cv2.setMouseCallback('Image', mouse_callback)
+            
+            while not rospy.is_shutdown():
+                rate.sleep()
+                if not test_node.can_run():
+                    continue
+
+                coord_3d_map, image = test_node.run()
+
+                # check pixel 3D coords in interactive mode
+                cv2.circle(image, center=(mouse_x, mouse_y), radius=5, color=(0, 0, 255), thickness=cv2.FILLED)
+                cv2.imshow('Image', image)
+                
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    exit(1)
+        except rospy.ROSInterruptException:
+            print('rospy node error.')
+
     print('\nDone!')
