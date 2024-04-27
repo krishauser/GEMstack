8ea0c30f4f8091feec3eb0839eee33f731761fc4
Zichenglong Wang <zw84@illinois.edu>
2024-04-27 12:37:37 -0500

    remove unused file

--- MLops/API/api_test.py
+++
@@ -1,3 +0,0 @@
-import mlops_api_gemstack as api
-
-api.list_datasets_info()
--- MLops/Backend/test_upload.py
+++
@@ -1,7 +0,0 @@
-import requests
-url = 'http://localhost:5000/models/upload'
-filepath = 'Schema.js'
-with open(filepath, 'rb') as f:
-    response = requests.post(url, files={'file': (filepath, f)})
-    print(response)
-
