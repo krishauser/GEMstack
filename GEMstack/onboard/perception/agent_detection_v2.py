8ab3eaa944a5ba38ae4b07be3deb5951203d5972
Vishakh Suresh Babu <vishakhsuresh98@gmail.com>
2024-05-02 02:56:16 -0500

    Updated agent detector

---
+++ GEMstack/onboard/perception/agent_detection_v2.py
@@ -0,0 +1,121 @@
+from ...state import AllState,VehicleState,ObjectPose,ObjectFrameEnum,AgentState,AgentEnum,AgentActivityEnum
+from ...utils import settings
+from ..interface.gem import GEMInterface
+from ..component import Component
+from .object_detection import ObjectDetector
+
+from ultralytics import YOLO
+from typing import Dict
+import threading
+import copy
+import cv2
+import numpy as np
+import time
+
+
+class AgentDetector(Component):
+    """Detects other agents (pedestrians + vehicles)."""
+
+    def __init__(self, vehicle_interface : GEMInterface):
+        self.vehicle_interface = vehicle_interface
+
+        self.camera_image = None
+        self.lidar_point_cloud = None
+        
+        self.detector = YOLO(settings.get('perception.agent_detection.model'))
+        
+        self.object_detector = None
+
+        self.agent_states = {}
+
+    def rate(self):
+        return settings.get('perception.rate')
+    
+    def state_inputs(self):
+        return ['vehicle']
+    
+    def state_outputs(self):
+        return ['agents']
+
+    def initialize(self):
+        # use image_callback whenever 'front_camera' gets a reading, and it expects images of type cv2.Mat
+        self.vehicle_interface.subscribe_sensor('front_camera', self.image_callback, cv2.Mat)
+        
+        # use lidar_callback whenever 'top_lidar' gets a reading, and it expects numpy arrays
+        self.vehicle_interface.subscribe_sensor('top_lidar', self.lidar_callback, np.ndarray)
+    
+    def image_callback(self, image : cv2.Mat):
+        self.camera_image = image
+
+    def lidar_callback(self, point_cloud: np.ndarray):
+        self.lidar_point_cloud = point_cloud
+    
+    def update(self, vehicle : VehicleState) -> Dict[str,AgentState]:
+        if self.camera_image is None or self.lidar_point_cloud is None:
+            return {}   # no image data or lidar data yet
+        
+        # debugging
+        # self.save_data()
+
+        self.object_detector = ObjectDetector(vehicle, self.camera_image, self.lidar_point_cloud, self.detector)
+
+        detected_agents = self.detect_agents()
+        return detected_agents
+
+    def object_to_agent(self, detected_object, bbox_cls):
+        """Creates a 3D agent state from a PhysicalObject."""
+
+        # set agent type based on class id
+        type_dict = {
+            '0': AgentEnum.PEDESTRIAN,
+            '1': AgentEnum.BICYCLIST,
+            '2': AgentEnum.CAR,
+            '7': AgentEnum.MEDIUM_TRUCK if detected_object.dimensions[2] <= 2.0 else AgentEnum.LARGE_TRUCK
+        }
+        return AgentState(pose=detected_object.pose, dimensions=detected_object.dimensions, outline=None, 
+                          type=type_dict[str(int(bbox_cls))], activity=AgentActivityEnum.STOPPED, 
+                          velocity=(0,0,0), yaw_rate=0, attributes=None)
+
+    def detect_agents(self):
+        yolo_class_ids = [
+            0,  # person
+            1,  # bicycle
+            2,  # car
+            7   # truck
+        ]
+        detected_objects, bbox_classes = self.object_detector.detect_objects(yolo_class_ids)
+
+        detected_agents = []
+        for i in range(len(detected_objects)):
+            agent = self.object_to_agent(detected_objects[i], bbox_classes[i])
+            detected_agents.append(agent)
+        
+        return detected_agents
+
+
+class OmniscientAgentDetector(Component):
+    """Obtains agent detections from a simulator"""
+    def __init__(self,vehicle_interface : GEMInterface):
+        self.vehicle_interface = vehicle_interface
+        self.agents = {}
+        self.lock = threading.Lock()
+
+    def rate(self):
+        return 4.0
+    
+    def state_inputs(self):
+        return []
+    
+    def state_outputs(self):
+        return ['agents']
+
+    def initialize(self):
+        self.vehicle_interface.subscribe_sensor('agent_detector',self.agent_callback, AgentState)
+    
+    def agent_callback(self, name : str, agent : AgentState):
+        with self.lock:
+            self.agents[name] = agent
+
+    def update(self) -> Dict[str,AgentState]:
+        with self.lock:
+            return copy.deepcopy(self.agents)
