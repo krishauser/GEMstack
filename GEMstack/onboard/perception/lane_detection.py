fcdb2f96d7ced46cdfae314fec1255f3139fa36f
Vishakh Suresh Babu <vishakhsuresh98@gmail.com>
2024-05-03 17:08:15 -0500

    Updated lane detection

--- GEMstack/onboard/perception/lane_detection.py
+++ GEMstack/onboard/perception/lane_detection.py
@@ -4,6 +4,7 @@ from ..interface.gem import GEMInterface
 from ..component import Component
 from .pixelwise_3D_lidar_coord_handler import PixelWise3DLidarCoordHandler
 
+import warnings
 import numpy as np
 import cv2
 import math
@@ -21,13 +22,8 @@ class LaneDetector(Component):
 
         self.camera_image = None
         self.height, self.width = None, None
+        
         self.lidar_point_cloud = None
-
-        self.left_pts = []
-        self.right_pts = []
-
-        self.left_poly = None # polynomial
-        self.right_poly = None
 
     def rate(self):
         return settings.get('perception.lane_detection.rate')
@@ -61,17 +57,18 @@ class LaneDetector(Component):
         
         t1 = timeit.default_timer()
 
-        detected_lane = self.detect_lanes()
+        left_poly, right_poly = self.detect_lane()
 
         t2 = timeit.default_timer()
         print('Detection time: {:.6f} s'.format(t2 - t1))
 
         r = copy.deepcopy(roadgraph)
 
-        if detected_lane:
-            r.lanes = {'lane0' : detected_lane}
+        if left_poly and right_poly:
+            # update roadgraph
+            r.lanes = {'lane0' : self.polys_to_roadgraph_lane(left_poly, right_poly)}
         
-        return r # updated roadgraph
+        return r 
 
     def region_of_interest(self, image):
         vertices = [
@@ -90,45 +87,69 @@ class LaneDetector(Component):
         if detected_lines is None:
             return
 
+        left_pts = []
+        right_pts = []
+
         for line in detected_lines:
             x1, y1, x2, y2 = line[0]
             m = (y2 - y1) / (x2 - x1) # slope
             
             if m < math.tan(-min_angle): # left
-                self.left_pts.extend([(x1, y1), (x2, y2)])
+                left_pts.extend([(x1, y1), (x2, y2)])
             elif m > math.tan(min_angle): # right
-                self.right_pts.extend([(x1, y1), (x2, y2)])
+                right_pts.extend([(x1, y1), (x2, y2)])
             else: # not part of the lane markings
                 pass
 
-    def compute_polys(self):
-        if len(self.left_pts) == 0 or len(self.right_pts) == 0:
+        return left_pts, right_pts
+
+    def compute_polys(self, left_pts, right_pts):
+        if len(left_pts) == 0 or len(right_pts) == 0:
             return
 
-        left_coeffs = np.polyfit([p[1] for p in self.left_pts], [p[0] for p in self.left_pts], deg=1)
-        self.left_poly = np.poly1d(left_coeffs)
+        left_coeffs = np.polyfit([p[1] for p in left_pts], [p[0] for p in left_pts], deg=1)
+        right_coeffs = np.polyfit([p[1] for p in right_pts], [p[0] for p in right_pts], deg=1)
 
-        right_coeffs = np.polyfit([p[1] for p in self.right_pts], [p[0] for p in self.right_pts], deg=1)
-        self.right_poly = np.poly1d(right_coeffs)
+        return np.poly1d(left_coeffs), np.poly1d(right_coeffs)
 
-    def polys_to_roadgraph_lane(self):
-        if not self.left_poly or not self.right_poly:
-            return
+    def detect_lane(self):
+        # convert color space
+        lab_image = cv2.cvtColor(self.camera_image, cv2.COLOR_RGB2LAB)
 
-        # Obtain 3d world coordinates for all pixels in the image
-        handler = PixelWise3DLidarCoordHandler()
-        all_points = handler.get3DCoord(self.camera_image, self.lidar_point_cloud) # img ht x img width x 3
+        # canny edge detection
+        edge_image = cv2.Canny(lab_image, 255/3, 255)
+
+        # specify a region of interest
+        cropped_image = self.region_of_interest(edge_image)
+
+        # detect straight lines
+        detected_lines = cv2.HoughLinesP(cropped_image, rho=20, theta=np.pi/60, threshold=300, 
+                                         lines=np.array([]), minLineLength=20, maxLineGap=100)
+
+        # compute equations for the left and right lane lines
+        left_pts, right_pts = self.separate_lines(detected_lines)
+        left_poly, right_poly = self.compute_polys(left_pts, right_pts) 
+
+        return left_poly, right_poly
+
+    def polys_to_roadgraph_lane(self, left_poly, right_poly):
+        with warnings.catch_warnings():
+            warnings.filterwarnings('ignore', category=UserWarning)
+            
+            # obtain 3d world coordinates for all pixels in the image
+            handler = PixelWise3DLidarCoordHandler()
+            all_points = handler.get3DCoord(self.camera_image, self.lidar_point_cloud) # img ht x img width x 3
 
         y_min = int(self.height * (1 - horizon))
         y_max = self.height
         
         # update y_max if the lane lines extend beyond the image
-        if self.left_poly(y_max) < 0 or self.right_poly(y_max) > self.width:
-            y_max = min(self.left_poly.roots, (self.right_poly - self.width).roots)
+        if left_poly(y_max) < 0 or right_poly(y_max) > self.width:
+            y_max = min(left_poly.roots, (right_poly - self.width).roots)
 
         centerline = []
         for j in range(y_max - 1, y_min - 1, -int((y_max - y_min) / 10)):
-            i = int((self.left_poly(j) + self.right_poly(j)) / 2)
+            i = int((left_poly(j) + right_poly(j)) / 2)
             if any(all_points[j][i]):
                 centerline.append(all_points[j][i])
 
@@ -144,24 +165,3 @@ class LaneDetector(Component):
                              surface=RoadgraphSurfaceEnum.PAVEMENT,
                              route_name='',
                              center=roadgraph_curve)
-
-    def detect_lanes(self):
-        # convert color space
-        lab_image = cv2.cvtColor(self.camera_image, cv2.COLOR_RGB2LAB)
-
-        # canny edge detection
-        edge_image = cv2.Canny(lab_image, 255/3, 255)
-
-        # specify a region of interest
-        cropped_image = self.region_of_interest(edge_image)
-
-        # detect straight lines
-        detected_lines = cv2.HoughLinesP(cropped_image, rho=20, theta=np.pi/60, threshold=300, 
-                                         lines=np.array([]), minLineLength=20, maxLineGap=100)
-
-        # compute equations for the left and right lane lines
-        self.separate_lines(detected_lines)
-        self.compute_polys() 
-
-        lane = self.polys_to_roadgraph_lane()
-        return lane
