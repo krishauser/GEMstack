c89ffd5754a6a40d95357168f822ebae9c7a9383
Paul Osuma <poo3@cornell.edu>
2024-05-03 22:14:28 -0500

    update on vio integration on Gemstack. Only files used for vio added to branch s2024

--- GEMstack/onboard/perception/state_estimation.py
+++ GEMstack/onboard/perception/state_estimation.py
@@ -11,6 +11,13 @@ from ..interface.gem import GEMInterface
 from ..component import Component
 from ..interface.gem_hardware import GNSSReading
 
+#necessary imports for processing Vio odometry information
+from nav_msgs.msg import Odometry
+from ...mathutils import transforms
+from ..interface.vioslam_reading import VioslamReading
+import rospy
+import numpy as np
+
 class GNSSStateEstimator(Component):
     """Just looks at the GNSS reading to estimate the vehicle state"""
     def __init__(self, vehicle_interface : GEMInterface):
@@ -59,6 +66,71 @@ class GNSSStateEstimator(Component):
 
         readings = self.vehicle_interface.get_reading()
         raw = readings.to_state(vehicle_pose_global)
+
+        #filtering speed
+        raw.v = self.gnss_speed
+        #filt_vel     = self.speed_filter(raw.v)
+        #raw.v = filt_vel
+        return raw
+    
+
+# class that updates the vehicle state object with poses from VIO odometry
+class VIOSlamEstimator(Component):
+    """Looks at the Vioslam reading to estimate the vehicle state"""
+    def __init__(self, vehicle_interface : GEMInterface):
+        self.vehicle_interface = vehicle_interface
+        self.Vioslam_pose = None
+        self.cam_location = settings.get('vehicle.calibration.front_camera.center_position')[:2]
+        self.P_c_cam = -1 * np.array(self.cam_location) #position of center of the vehicle relative to camera 
+        self.speed_filter  = OnlineLowPassFilter(1.2, 30, 4)
+        self.status = None
+
+        self.Vioslam_sub = rospy.Subscriber("/Odom", Odometry, self.callback_with_Vioslam_reading)
+
+    def vio_slam_callback(self, reading : VioslamReading):
+        self.Vioslam_pose = reading.pose
+        self.status = reading.status
+
+    # Get information from the visual odometry topic from rtabmap
+    def callback_with_Vioslam_reading(self, msg : Odometry):
+        # position
+        x=msg.pose.pose.position.x
+        y=msg.pose.pose.position.y
+        z=msg.pose.pose.position.z
+        # orientation quaternion
+        xw = msg.pose.pose.orientation.x
+        yw = msg.pose.pose.orientation.y
+        zw = msg.pose.pose.orientation.z
+        w = msg.pose.pose.orientation.w
+        [roll, pitch, yaw] = transforms.quaternion_to_euler(xw, yw, zw, w)
+        start_pose = ObjectPose(ObjectFrameEnum.START,t = self.time(),x=x,y=y,z=z,yaw=yaw,roll=roll,pitch=pitch)
+        self.vio_slam_callback(VioslamReading(start_pose,'ok'))
+
+    def rate(self):
+        return 10.0
+    
+    def state_outputs(self) -> List[str]:
+        return ['vehicle']
+
+    def healthy(self):
+        return self.Vioslam_pose is not None
+    
+    def rotation_mat(self, yaw):
+        return np.array([[math.cos(yaw), -math.sin(yaw)], [math.sin(yaw), math.cos(yaw)]])
+
+    def update(self) -> VehicleState:
+        if self.Vioslam_pose is None:
+            return
+        # reference point is located at the center of rear axle
+        center_x, center_y = np.array([self.Vioslam_pose.x, self.Vioslam_pose.y]) + \
+                        self.rotation_mat(self.Vioslam_pose.yaw) @ self.P_c_cam
+        print("VIO pose_start x, y, yaw = ", center_x, center_y, self.Vioslam_pose.yaw)
+        
+        self.Vioslam_pose.x=center_x
+        self.Vioslam_pose.y=center_y
+
+        readings = self.vehicle_interface.get_reading()
+        raw = readings.to_state(self.Vioslam_pose)
 
         #filtering speed
         raw.v = self.gnss_speed
