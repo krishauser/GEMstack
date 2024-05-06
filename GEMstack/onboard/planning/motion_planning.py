c277e62ab389c51f6c56246eef653d4be375c7a5
olivia22yhl <142858998+olivia22yhl@users.noreply.github.com>
2024-05-06 14:43:33 -0500

    Update motion_planning.py
    Added a new class SpeedAdjustTrajectoryPlanner.

--- GEMstack/onboard/planning/motion_planning.py
+++ GEMstack/onboard/planning/motion_planning.py
@@ -21,4 +21,23 @@ class RouteToTrajectoryPlanner(Component):
         if self.reference_speed is None:
             return state.route
         return state.route.arc_length_parameterize(self.reference_speed)
+
+class SpeedAdjustTrajectoryPlanner(Component):
+    """Copies the route directly into the trajectory."""
+    def __init__(self, reference_speed = 1.0):
+        self.reference_speed = reference_speed
+
+    def state_inputs(self):
+        return ['all']
+
+    def state_outputs(self) -> List[str]:
+        return ['trajectory']
+
+    def rate(self):
+        return 10.0
+
+    def update(self, state : AllState):
+        if self.reference_speed is None:
+            return state.route
+        return state.route.var_speed_arc_length_parameterize(self.reference_speed)
     
