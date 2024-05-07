8cb7d6e7a5b048cff506f3f8a687c273f9f298b6
Enguang Fan <enguang2@illinois.edu>
2024-05-07 08:51:34 -0500

    done with GNSS/IMU EKF

--- GEMstack/onboard/perception/state_estimation.py
+++ GEMstack/onboard/perception/state_estimation.py
@@ -13,8 +13,6 @@ from ..interface.gem_hardware import GNSSReading
 from ..interface.gem_hardware import IMUReading
 import numpy as np
 from ekf import ESKF 
-from ...mathutils.transforms import Quaternion
-from ...mathutils.units import EARTH_RADIUS
 
 
 class EKFStateEstimator(Component):
@@ -32,7 +30,7 @@ class EKFStateEstimator(Component):
         self.yaw_offset = settings.get('vehicle.calibration.gnss_yaw')
         self.speed_filter  = OnlineLowPassFilter(1.2, 30, 4)
         self.status = None
-        self.long_ref, self.lat_ref = -1.540009687915658, 0.6997620706897224
+        self.long_ref, self.lat_ref = -1.540009687915658, 0.6997620706897224 #a location outside of highbay as reference point 
 
 
         #initialize ESKF estimator
@@ -83,17 +81,13 @@ class EKFStateEstimator(Component):
                                       yaw=center_xyhead[2])
 
         readings = self.vehicle_interface.get_reading()
-
-        #TODO, remove constant
-        # self.initialize_converter(-1.540009687915658, 0.6997620706897224)
-        # self.to_cartesian(vehicle_pose_global.x, vehicle_pose_global.y, vehicle_pose_global.yaw)
         
+        #Transform from latitude/longitude to Cartesian coordinates in meters
         self.dx, self.dy = transforms.lat_lon_to_xy(vehicle_pose_global.x, vehicle_pose_global.y, self.lat_ref, self.long_ref)
-
 
         #propagate and update of the kalman filter
         self.estimator.propagate(self.imu_reading.linear_acceleration, self.imu_reading.angular_velocity, self.vehicle_interface.time())
-        self.estimator.measurement_update(var_gnss, np.array([self.dx, self.dy, vehicle_pose_global.z]))
+        self.estimator.measurement_update(0.000025, np.array([self.dx, self.dy, vehicle_pose_global.z]))
 
         vehicle_pose_global.x, vehicle_pose_global.y = transforms.xy_to_lat_lon(self.estimator.p[0], self.estimator.p[1], self.lat_ref, self.long_ref)
         raw = readings.to_state(vehicle_pose_global)
@@ -104,60 +98,6 @@ class EKFStateEstimator(Component):
         #filt_vel     = self.speed_filter(raw.v)
         #raw.v = filt_vel
         return raw
-    
-    def initialize_converter(self, init_lon, init_lat):
-        #customized initial point
-        """ Store initial longitude and latitude converted to degrees """
-        self.init_lon = math.degrees(init_lon)
-        self.init_lat = math.degrees(init_lat)
-        # return init_lon, init_lat
-    
-    def to_cartesian(self, lon, lat, yaw):
-        """ Convert geographical coordinates to Cartesian coordinates """
-        # Convert input coordinates from radians to degrees
-        
-        lon = math.degrees(lon)
-        lat = math.degrees(lat)
-
-        # Calculate differences in coordinates
-        dlat = math.radians(lat - self.init_lat)
-        dlon = math.radians(lon - self.init_lon)
-        a = self.init_lat * math.pi / 180
-
-        # Convert differences in latitude and longitude to distances in meters
-        dx = dlon * earth_radius * math.cos(a)
-        dy = dlat * earth_radius
-
-        self.dx = dx
-        self.dy = dy
-        self.dyaw = yaw
-    
-    def to_geographical(self, dx, dy, yaw):
-        """Convert Cartesian coordinates back to geographical coordinates."""
-        # Latitude and longitude differences in radians
-        dlat = dy / earth_radius
-        a = self.init_lat * math.pi / 180
-        dlon = dx / (earth_radius * math.cos(a))
-
-        # Convert differences to degrees and apply to the initial latitude and longitude
-        new_lat = self.init_lat + math.degrees(dlat)
-        new_lon = self.init_lon + math.degrees(dlon)
-
-        # Return new coordinates in radians and unchanged yaw
-        new_lon = math.radians(new_lon)
-        new_lat = math.radians(new_lat)
-
-        return new_lon, new_lat, yaw
-
-        # Return the differences as new (x, y) coordinates and the unchanged yaw
-        #pass
-        #return dx, dy, yaw
-    
-    # Example usage
-    # initial_lon, initial_lat = initialize_converter(-1.540009687915658, 0.6997620706897224)
-    # x, y, yaw = to_cartesian(initial_lon, initial_lat, -1.540009687915658, 0.6997620706897224, 2.792526766781833)
-    # print(f"X: {x:.2f}, Y: {y:.2f}, Yaw: {yaw:.2f}")
-
         
 
 class GNSSStateEstimator(Component):
