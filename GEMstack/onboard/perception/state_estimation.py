1a646451b4e9f3f9511e0a86aff35c5bfe73cdbe
Enguang Fan <enguang2@illinois.edu>
2024-05-07 09:28:39 -0500

    try to resolve code duplication error

--- GEMstack/onboard/perception/state_estimation.py
+++ GEMstack/onboard/perception/state_estimation.py
@@ -18,11 +18,9 @@ class EKFStateEstimator(Component):
     """Use IMU and GNSS reading to jointly estimate the vehicle state"""
     def __init__(self, vehicle_interface : GEMInterface):
         self.vehicle_interface = vehicle_interface
-        if 'gnss' not in vehicle_interface.sensors():
-            raise RuntimeError("GNSS sensor not available")
+        if 'gnss' not in vehicle_interface.sensors() or 'imu' not in vehicle_interface.sensors():
+            raise RuntimeError("Septentrio INS receiver not available")
         vehicle_interface.subscribe_sensor('gnss',self.gnss_callback,GNSSReading)
-        if 'imu' not in vehicle_interface.sensors():
-            raise RuntimeError("IMU sensor not available")
         vehicle_interface.subscribe_sensor('imu',self.imu_callback)
         self.gnss_pose = None
         self.location = settings.get('vehicle.calibration.gnss_location')[:2]
@@ -64,12 +62,10 @@ class EKFStateEstimator(Component):
     def update(self) -> VehicleState:
         if self.gnss_pose is None:
             return
-        #TODO: figure out what this status means
-        #print("INS status",self.status)
 
-        # vehicle gnss heading (yaw) in radians
-        # vehicle x, y position in fixed local frame, in meters
-        # reference point is located at the center of GNSS antennas
+        # vehicle gnss heading not working on Gem e4 vehicle
+        # vehicle x, y position in radians, global frame, see gem_hardware.py
+        # callback_with_gnss_reading sets to global frame
         localxy = transforms.rotate2d(self.location,-self.yaw_offset)
         gnss_xyhead_inv = (-localxy[0],-localxy[1],-self.yaw_offset)
         center_xyhead = self.gnss_pose.apply_xyhead(gnss_xyhead_inv)
@@ -90,12 +86,7 @@ class EKFStateEstimator(Component):
 
         vehicle_pose_global.x, vehicle_pose_global.y = transforms.xy_to_lat_lon(self.estimator.p[0], self.estimator.p[1], self.lat_ref, self.long_ref)
         raw = readings.to_state(vehicle_pose_global)
-
-
-        #filtering speed
         raw.v = self.gnss_speed
-        #filt_vel     = self.speed_filter(raw.v)
-        #raw.v = filt_vel
         return raw
         
 
