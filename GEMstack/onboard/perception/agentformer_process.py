a9bc471f6635142398c5537ea9cb2ae2209b8e21
Ananya <14322650+ananyeet@users.noreply.github.com>
2024-05-08 16:36:40 -0500

    Visualize agentformer?

--- GEMstack/onboard/perception/agentformer_process.py
+++ GEMstack/onboard/perception/agentformer_process.py
@@ -9,13 +9,14 @@ import subprocess
 import shutil
 import sys, copy
 import time
+from typing import Dict
 import torch
 import pandas as pd
 import numpy as np
 from collections import defaultdict
 
 # path = "GEMstack/onboard/prediction/agentformer"
-sys.path.append('GEMstack/onboard/perception/agentformer')
+sys.path.append('perception/agentformer')
 from utils.torch import *
 from utils.config import Config
 from model.model_lib import model_dict
@@ -53,6 +54,7 @@ def run_model(model, cfg, gt_data):
     torch.save(sample_motion_3D, 'sample_motion.pt')
     torch.save(valid_id, 'valid_id.pt')
     torch.save(frame, 'frame.pt')
+    return sample_motion_3D, valid_id, frame
 
 def run_model_on_data(generator, save_dir, cfg, model, device, log):
     total_num_pred = 0
@@ -69,6 +71,117 @@ def run_model_on_data(generator, save_dir, cfg, model, device, log):
         sample_motion_3D = sample_motion_3D * cfg.traj_scale
     
         return sample_motion_3D, data['valid_id'], frame
+    
+# {framenum: {ped_id: {x or y: float}}}
+def convert_data_to_model_input(past_agent_states: Dict[int, Dict[int, Dict[str, float]]]) -> np.ndarray:
+    # get tracked frames for pedestrian agents
+    pedestrian_agent_states = past_agent_states
+    if len(pedestrian_agent_states) == 0:
+        return []
+
+
+    # get the 8 most recent frames(highest frame number) from the past_agent_states
+    past_agent_states = []
+
+    # sort pedestrian agent states by frame number
+    for frame in sorted(pedestrian_agent_states.keys(), reverse=True)[:NUM_PREV_FRAMES+1]:
+        past_agent_states.append(pedestrian_agent_states[frame])
+    # reverse past_agent_states so that the most recent frame is first
+    past_agent_states = past_agent_states[::-1]
+    # past_agent_states = [{ped_id: state}] * # of frames 
+
+
+    # takes in list of dictionaries corresponding to past 8 frames
+    past_frames = []
+    recent_pids = {}
+
+    # Dummy frames should be written for these pids
+    valid_pids = set()
+    for framenum in range(len(past_agent_states)):
+        frame = past_agent_states[framenum]
+        for ped_id, agent_state in frame.items():
+            # create length 17 aray of -1
+            row = np.full(17, -1.0)
+            # get x, y
+            x, y = agent_state["x"], agent_state["y"]
+            row[0] = framenum
+            row[1] = ped_id
+            
+            pid_seen_count = recent_pids.get(ped_id, 0) + 1
+            if pid_seen_count >= 8:
+                valid_pids.add(ped_id)
+                    
+            recent_pids[ped_id] = pid_seen_count 
+            # we need to flip the x and y coordinates for the model
+            row[-4] = y
+            row[-2] = x
+            
+            past_frames.append(row)
+            
+
+    if len(valid_pids) == 0:
+        return []
+
+    for frame in range(NUM_PREV_FRAMES + 1, NUM_PREV_FRAMES + 1 + NUM_FUTURE_FRAMES):
+        for rpid in valid_pids:
+            # create length 17 aray of -1
+            row = np.full(17, -1.0)
+            # get x, y
+            x, y = -1.0, -1.0
+            row[0] = frame
+            row[1] = rpid
+            # we need to flip the x and y coordinates for the model
+            row[-4] = y
+            row[-2] = x
+            past_frames.append(row)
+
+
+    past_frames = np.array(past_frames).astype(str)
+    past_frames[:,2] = "Pedestrian"
+
+    np.savetxt(INPUT_FILE_PATH, past_frames, delimiter=" ", fmt="%s")
+
+    return past_frames
+
+# output:
+# [{ped_id: {x or y: float}}]
+def convert_data_from_model_output(self, sample_model_3D, valid_id, frame):
+    agent_list = []
+    # sample_model_3D: 3 x ped_id x 12 x 2
+    iterations = 0
+    for traj in range(sample_model_3D.shape[0]):
+        # starttime = time.time()
+        # Create the dictionary of pedestrian-future AgentState lists for the current trajectory
+        agent_dict = defaultdict(list) # Key: Pedestrian ID | Value: List of AgentStates for each future frame
+        for ped_idx in range(sample_model_3D.shape[1]):
+            for future_frame_id in range(sample_model_3D.shape[2]):
+                ped_id = valid_id[ped_idx]
+                # flip the x- and y-coordinates back to normal
+                y, x = sample_model_3D[traj][ped_idx][future_frame_id]
+                agent_state = {"x": x, "y": y}
+                # convert the frame_id to time
+                # frame_id = frame + future_frame_id + 1
+                # frame_time = frame_id / self.frame_rate + self.cur_time
+
+
+                # # create an AgentState object
+                # pose = ObjectPose(t=frame_time, x=x, y=y, z=0, yaw=0, pitch=0, roll=0, frame=ObjectFrameEnum.START)
+
+                # # dimensions of a pedestrian (not accurate)
+                # dims = PEDESTRIAN_DIMS
+                # # velocity = esimate velocity from past few frames
+                # # velocity = self.estimate_velocity(past few frames)
+                # agent_state = AgentState(pose=pose, dimensions=dims, outline=None, type=AgentEnum.PEDESTRIAN, activity=AgentActivityEnum.MOVING, velocity=(0, 0, 0), yaw_rate=0)
+                agent_dict[ped_id].append(agent_state)
+                iterations += 1
+            # print("single pedestrian time", time.time()-starttime)
+        agent_list.append(agent_dict)
+        # print(starttime-time.time(), "iteration", traj)
+
+    # print(time.time() - starttime, "line 245")
+    # print(iterations, "iterations of innermost loop")
+    return agent_list
+
 
 if __name__ == "__main__":
     config_file = 'cfg/eth_ucy/inference_data/inference.yml'
