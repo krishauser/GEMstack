84e25c3421fcc6ce39c79e27dc849a2fe22f5d94
Ananay Mathur <ananaymathur@gmail.com>
2024-05-08 13:26:36 -0500

    Added extra documentation to readme and the script

--- GEMstack/offboard/detection_learning/traffic_sign_and_light_model_train.py
+++ GEMstack/offboard/detection_learning/traffic_sign_and_light_model_train.py
@@ -4,8 +4,9 @@ model = YOLO("yolov8m.pt")
 model.train(data="data.yaml", epochs=50)
 metrics = model.val()
 
-metrics.box.map    # map50-95
-metrics.box.map50  # map50
-metrics.box.map75  # map75
-metrics.box.maps   # a list contains map50-95 of each category
+print("Overall mAP:", metrics.box.map)
+print("mAP at IoU threshold 0.50:", metrics.box.map50)
+print("mAP at IoU threshold 0.75:", metrics.box.map75)
+print("mAP values at different IoU thresholds for each category:", metrics.box.maps)
+
 
