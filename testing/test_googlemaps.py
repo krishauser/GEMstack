f06178c4cd8453d67908969336bab762b2f802d9
OneMaple <37830933+onemapl3@users.noreply.github.com>
2024-05-08 16:59:48 -0500

    Revert "S2024 curbside googlemap"

--- testing/test_googlemaps.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-sys.path.append(os.getcwd())
-from GEMstack.onboard.googlemaps import conversion
-
-Location = input("Where would you like to go? ")
-conversion.test_find_place(Location)
