a1e1bc4d1bc8a8ae005e4907995bbb24e650697b
hermya <herambku@gmail.com>
2024-05-08 20:11:42 -0500

    Figured out necessary modifications for CARLA + pure pursuit

--- GEMstack/onboard/planning/pure_pursuit.py
+++ GEMstack/onboard/planning/pure_pursuit.py
@@ -83,8 +83,12 @@ class PurePursuit(object):
             if self.trajectory.frame != state.pose.frame:
                 print("Transforming trajectory from",self.trajectory.frame.name,"to",state.pose.frame.name)
                 self.trajectory = self.trajectory.to_frame(state.pose.frame, current_pose=state.pose)
-
-        closest_dist,closest_parameter = self.path.closest_point_local((curr_x,curr_y),[self.current_path_parameter-5.0,self.current_path_parameter+5.0])
+        low = 5.0
+        high = 5.0
+        """
+        MAKE SURE TO USE low = 0.0 AND high = 20.0 WHEN USING CARLA SIMULATION (TODO shift this to file)
+        """
+        closest_dist,closest_parameter = self.path.closest_point_local((curr_x,curr_y),[self.current_path_parameter - low,self.current_path_parameter + high])
         self.current_path_parameter = closest_parameter
         self.current_traj_parameter += dt
         #TODO: calculate parameter that is look_ahead distance away from the closest point?
