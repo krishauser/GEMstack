79616f37676068e78055ede74bc2b706156f61b6
Colin Lane <clane@992-911-turbo-s.local>
2025-02-01 17:13:18 -0600

    added publisher to blink.py

--- homework/blink.py
+++ homework/blink.py
@@ -2,6 +2,10 @@ import rospy
 from std_msgs.msg import String, Bool, Float32, Float64
 from pacmod_msgs.msg import PositionWithSpeed, PacmodCmd, SystemRptInt, SystemRptFloat, VehicleSpeedRpt
 
+TURN_RIGHT = 0
+TURN_NONE = 1
+TURN_LEFT = 2
+TURN_HAZARD = 3 
 # For message format, see
 # https://github.com/astuff/astuff_sensor_msgs/blob/3.3.0/pacmod_msgs/msg/PacmodCmd.msg
 
@@ -17,9 +21,15 @@ class BlinkDistress:
         # ROS message type, and you can find out what this is by either reading the documentation or running
         # "rostopic info /pacmod/parsed_tx/X" on the command line.
 
+        self.turn_blink_pub = rospy.Publisher('/pacmod/as_rx/turn_cmd', PacmodCmd, queue_size=1)
+        #define blink cmd
+        self.turn_cmd = PacmodCmd()
+        self.turn_cmd.ui16_cmd = TURN_NONE 
+        self.turn_blink_pub.publish(self.turn_cmd)
+
         # Subscribers to /pacmod/parsed_tx/X (at least 2)
-        self.sub = rospy.Subscriber("/pacmod/parsed_tx/vehicle_speed_rpt", VehicleSpeedRpt, self.speed_callback)
-        self.sub = rospy.Subscriber("/pacmod/parsed_tx/accel_rpt", SystemRptFloat, self.accel_callback)
+        self.speed_sub = rospy.Subscriber("/pacmod/parsed_tx/vehicle_speed_rpt", VehicleSpeedRpt, self.speed_callback)
+        self.accel_sub = rospy.Subscriber("/pacmod/parsed_tx/accel_rpt", SystemRptFloat, self.accel_callback)
         
         pass
 
@@ -34,12 +44,25 @@ class BlinkDistress:
     def cleanup(self):
         """Run last"""
         pass
+
+    def get_dir_distress(self):
+        pass
     
     def update(self):
         """Run in a loop"""
         # TODO: Implement your control loop here
         # You will need to publish a PacmodCmd() to /pacmod/as_rx/turn_cmd.  Read the documentation to see
         # what the data in the message indicates.
+        #self.turn_cmd = PacmodCmd()
+        # TODO change to actual direction in Part 2
+        if self.turn_cmd.ui16_cmd == TURN_NONE:
+            self.turn_cmd.ui16_cmd == TURN_LEFT
+        elif self.turn_cmd.ui16_cmd == TURN_LEFT:
+            self.turn_cmd.ui16_cmd == TURN_RIGHT
+        else:
+            self.turn_cmd.ui16_cmd == TURN_NONE
+        self.turn_blink_pub.publish(self.turn_cmd)
+
         pass
        
     def healthy(self):
