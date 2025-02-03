235f58a8111afd40b92e05fcfa2930bee30b0f05
joe-hung <hyhung3@illinois.edu>
2025-02-03 12:39:51 -0600

    1st_version_for_sim

--- homework/blink_component.py
+++ homework/blink_component.py
@@ -1,8 +1,7 @@
 from ..component import Component
 from ..interface.gem import GEMInterface,GEMVehicleCommand,GEMVehicleReading
 
-from pacmod_msgs import SystemRptFloat
-import rospy
+import time
 
 class BlinkDistress(Component):
     """Your control loop code should go here.  You will use GEMVehicleCommand
@@ -12,7 +11,14 @@ class BlinkDistress(Component):
         self.vehicle_interface = vehicle_interface
         self.command = None
         
-        self.accel_sub = rospy.Subscriber("parsed_tx/accel_rpt",SystemRptFloat,self.cb_accel,queue_size=1)
+        self.prev_time = time.time()
+        self.curr_time = time.time()
+               
+        while True :
+            self.curr_time = time.time()
+            if (self.prev_time - self.curr_time > 2):
+                self.update()
+                self.prev_time = self.curr_time
 
     def rate(self):
         """Requested update frequency, in Hz"""
@@ -36,6 +42,20 @@ class BlinkDistress(Component):
         # TODO: alter command to execute turn signals, then uncomment line below to send
         # the command to vehicle
         # self.vehicle_interface.send_command(command)
+        
+        if command.left_turn_signal is True:
+            command.left_turn_signal = False
+            command.right_turn_signal = True
+            
+        elif command.right_turn_signal is True:
+            command.left_turn_signal = False
+            command.right_turn_signal = False
+            
+        else:
+            command.left_turn_signal = True
+            command.right_turn_signal = False
+        
+        self.vehicle_interface.send_command(command)
        
     def healthy(self):
         """Returns True if the element is in a stable state."""
