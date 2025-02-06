30be8794ba6e950e1f6966de7db7eeb00fbe83e5
harishkumarbalaji <harishkumarbalaji@gmail.com>
2025-02-06 00:18:33 -0600

    Fix setup machine script and add volume mounts for dbus

--- run_docker_container.sh
+++ run_docker_container.sh
@@ -3,6 +3,6 @@
 if [ "$(docker ps -q -f name=gem_stack-container)" ]; then
     docker exec -it gem_stack-container bash
 else
-    docker compose -f setup/docker-compose.yaml up -d
+    UID=$(id -u) GID=$(id -g) docker compose -f setup/docker-compose.yaml up -d
     docker exec -it gem_stack-container bash
 fi
