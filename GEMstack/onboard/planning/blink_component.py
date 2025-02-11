3524597f4057ae4f4ff6de4efc85d3d5c6766290
GEM e4 user <gem@gem-nuvo.com>
2025-02-11 17:18:57 -0600

    hw part 1.1 1.2 finish

--- GEMstack/onboard/planning/blink_component.py
+++ GEMstack/onboard/planning/blink_component.py
@@ -1,26 +1,31 @@
 from ..component import Component
 from ..interface.gem import GEMInterface,GEMVehicleCommand,GEMVehicleReading
+from ..interface.gem_hardware import GEMHardwareInterface
+
 import time
-from ..state import AllState, VehicleIntentEnum
 
 class BlinkDistress(Component):
     """Your control loop code should go here.  You will use GEMVehicleCommand
     to communicate with drive-by-wire system to control the vehicle's turn signals.
     """
-    def __init__(self, vehicle_interface : GEMInterface):
+    def __init__(self, vehicle_interface : GEMHardwareInterface):
         self.vehicle_interface = vehicle_interface
-        # command = GEMVehicleCommand()
-
-        # command.left_turn_signal = True
+        self.command = GEMVehicleCommand(gear=0,
+                                accelerator_pedal_position=0,
+                                brake_pedal_position=0,
+                                steering_wheel_angle=0,
+                                accelerator_pedal_speed=0,
+                                brake_pedal_speed = 0,
+                                steering_wheel_speed = 0)
+        
         self.prev_time = time.time()
-        self.state = AllState()
-
-        while True :
-            # if(self.state.intent.intent == VehicleIntentEnum.HALTING)
-            #     self.update()
-            self.update()
-        
-        
+        self.curr_time = time.time()
+               
+        # while True :
+        #     self.curr_time = time.time()
+        #     if (self.curr_time - self.prev_time > 2):
+        #         self.update()
+        #         self.prev_time = self.curr_time
 
     def rate(self):
         """Requested update frequency, in Hz"""
@@ -40,44 +45,35 @@ class BlinkDistress(Component):
         # sent to the drive-by-wire system, simultaneously.  To avoid doing arbitrary things
         # to the vehicle, let's maintain the current values (e.g., accelerator, brake pedal,
         # steering angle) from its current readings.
-        command = self.vehicle_interface.command_from_reading()
+        # command = self.vehicle_interface.command_from_reading()
+        command = self.command
+        # print(command.left_turn_signal, command)
         # TODO: alter command to execute turn signals, then uncomment line below to send
         # the command to vehicle
         # self.vehicle_interface.send_command(command)
-
-        # print("curr_accel is {}".format(command.accelerator_pedal_position))
-        # print("curr_brake is {}".format(command.brake_pedal_position))
-        # print("right turn {}".format(command.right_turn_signal))
-        # print("left turn {}".format(command.left_turn_signal))
         self.curr_time = time.time()
-
-        if (self.curr_time - self.prev_time > 2):
-            # print("right turn {}".format(command.right_turn_signal))
-            # print("left turn {}".format(command.left_turn_signal))
-            if(command.left_turn_signal is True):
+        
+        if(self.curr_time - self.prev_time > 2):
+            if command.left_turn_signal is True:
+                # print("code goes if")
                 command.left_turn_signal = False
                 command.right_turn_signal = True
-                print("Condition 1")
-            elif (command.right_turn_signal is True):
+                
+            elif command.right_turn_signal is True:
+                # print("code goes elif")
                 command.left_turn_signal = False
                 command.right_turn_signal = False
-                print("Condition 2")
+                
             else:
+                # print("code goes else")
                 command.left_turn_signal = True
-                print("Condition 3")
+                command.right_turn_signal = False
+            
             self.prev_time = self.curr_time
-            # print("theoretical right turn {}".format(command.right_turn_signal))
-            # print("theoretical left turn {}".format(command.left_turn_signal))
-            # print("=========================================")
-            # print("real right turn {}".format(command.right_turn_signal))
-            # print("real left turn {}".format(command.left_turn_signal))
-
-            self.vehicle_interface.send_command(command)
-
+            self.command = command
         
-        # command = self.vehicle_interface.command_from_reading()
-        # print("real right turn {}".format(command.right_turn_signal))
-        # print("real left turn {}".format(command.left_turn_signal))
+        self.vehicle_interface.send_command(command)
+        # print(command.left_turn_signal, command.right_turn_signal)
        
     def healthy(self):
         """Returns True if the element is in a stable state."""
