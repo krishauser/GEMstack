a839a4b51e1434fb01e282986ef6f17d921652fa
AadarshHegde123 <ahegd2@illinois.edu>
2025-04-01 22:44:20 -0700

    updates to parking entry point to handle found parking spot signal

--- GEMstack/onboard/parking_entry_point.py
+++ GEMstack/onboard/parking_entry_point.py
@@ -2,6 +2,7 @@ from pacmod_msgs.msg import PositionWithSpeed, PacmodCmd, VehicleSpeedRpt
 import rospy
 from sensor_msgs.msg import Image,PointCloud2
 import sensor_msgs.point_cloud2 as pc2
+from std_msgs.msg import Float32MultiArray
 import ctypes
 import struct
 import argparse
@@ -9,11 +10,21 @@ import time
 
 
 speed = 0
+detected_space = False
+endpoints = []
 
 def speed_callback(msg):
     global speed
     speed = msg.vehicle_speed
     rospy.loginfo(f"Vehicle Speed: {msg.vehicle_speed} m/s | Valid: {msg.vehicle_speed_valid}")
+
+def parking_space_callback(msg):
+    global detected_space
+    global endpoints
+    detected_space = true
+    endpoints = msg
+    rospy.loginfo(f"Vehicle Speed: {msg.vehicle_speed} m/s | Valid: {msg.vehicle_speed_valid}")
+
 
 def main():
 
@@ -32,34 +43,23 @@ def main():
     brake_cmd.f64_cmd = 1
 
     speed_sub = rospy.Subscriber('/pacmod/parsed_tx/vehicle_speed_rpt', VehicleSpeedRpt, speed_callback)
+    detected_space_sub = rospy.Subscriber('/detected_space', Float32MultiArray, parking_space_callback)
 
-    while True:
-        # accel_cmd.f64_cmd = 0.4
-        # accel_pub.publish(accel_cmd)
-        if speed >= 1.5:
+    while not detected_space: # State machine
+        if speed >= 1.5: # let off of gas
             accel_cmd.f64_cmd = 0
             accel_pub.publish(accel_cmd)
-        if speed <= 1:
+        if speed <= 1: # accelerate until our speed is 1 m/s
             accel_cmd.f64_cmd = 0.4
             accel_pub.publish(accel_cmd)
 
-
-            # brake_cmd.f64_cmd = 0.5
-            # brake_pub.publish(brake_cmd)
     
-    
+    while speed > 0:
+        brake_cmd.f64_cmd = 0.3
+        brake_pub.publish(brake_cmd)
 
+    # Call a* planning logic
 
-
-    # time.sleep(5)
-
-    # brake_pub = rospy.Publisher('/pacmod/as_rx/brake_cmd', PacmodCmd, queue_size=1)
-    # brake_cmd = PacmodCmd()
-    # brake_cmd.enable = True
-    # brake_cmd.clear  = False
-    # brake_cmd.ignore = False
-    # brake_cmd.f64_cmd = 1
-    # brake_pub.publish(brake_cmd)
 
 
 if __name__ == "__main__":
