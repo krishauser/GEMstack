69675a9160155e75cd92eb108815e7aae4dfdff6
rjsun-KA <92330153+rjsun06@users.noreply.github.com>
2025-04-03 00:16:49 +0000

    ok

--- GEMstack/offboard/calibration/run_img2pc.bash
+++ GEMstack/offboard/calibration/run_img2pc.bash
@@ -1,8 +1,8 @@
 root='/mnt/GEMstack'
 
 python3 $root/GEMstack/offboard/calibration/img2pc.py \
-    --img_path $root/data/color32.png \
-    --pc_path $root/data/lidar32.npz \
+    --img_path $root/data/calib1/img/fl/fl11.png \
+    --pc_path $root/data/calib1/pc/ouster11.npz \
     --pc_transform_path $root/GEMstack/knowledge/calibration/gem_e4_ouster.yaml \
     --img_intrinsics_path $root/GEMstack/knowledge/calibration/gem_e4_oak_in.yaml \
     --n_features 4 \
