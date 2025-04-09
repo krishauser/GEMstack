4baf8519e3200a19749d4dedb26e42e57e40ff38
Tianshun Gao <tgao13@illinois.edu>
2025-04-08 23:09:22 -0700

    add support for log replay

--- GEMstack/onboard/visualization/sr_viz/threeD/utils/constants.js
+++ GEMstack/onboard/visualization/sr_viz/threeD/utils/constants.js
@@ -9,3 +9,5 @@ export const HUMAN_TO_CAR_HEIGHT_RATIO = 1.2;
 export const CAR_MODEL_PATH = "/models/Cadillac+CT4+V+2022.glb";
 export const TRAFFIC_LIGHT_MODEL_PATH = "/models/Traffic Light_01.glb";
 export const STREET_LIGHT_MODEL_PATH = "/models/StreetLightPoles.glb";
+export const ENV_MAP_PATH = "/textures/env/";
+export const ENV_MAP_NAME = "Tropical_Ruins.hdr";
