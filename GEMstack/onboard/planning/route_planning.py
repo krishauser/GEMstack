18e46ab3c5d9a0a8bff25100f21e8594179c5a8f
animesh <animeshsingh98@gmail.com>
2025-04-15 18:35:00 -0500

    Including heading in the route calculated

--- GEMstack/onboard/planning/route_planning.py
+++ GEMstack/onboard/planning/route_planning.py
@@ -4,6 +4,7 @@ from ...utils import serialization
 from ...state import Route,ObjectFrameEnum
 import os
 import numpy as np
+import math
 
 class StaticRoutePlanner(Component):
     """Reads a route from disk and returns it as the desired route."""
@@ -85,23 +86,46 @@ def max_visible_arc(circle_center, radius, geofence):
         return []
 
     max_arc = max(arc_segments, key=len)
+    for i in range(max_arc):
+        max_arc[i].append(heading_on_circle(xc,yc,max_arc[0], max_arc[1]))
     return max_arc
+
+def heading_on_circle(cx, cy, px, py):
+    dx = px - cx
+    dy = py - cy
+    tx = -dy
+    ty = dx
+    return math.atan2(ty, tx)  # Heading in radians
+
+
+def check_point_exists(server_url="http://localhost:8000"):
+    try:
+        response = requests.get(f"{server_url}/point")
+        response.raise_for_status()
+        points = response.json().get("points", [])
+        
+        for point in points:
+            return True, [[point[0]["x"], point[0]["y"]],[point[1]["x"], point[1]["y"]]]
+        return False, []
+
+    except requests.exceptions.RequestException as e:
+        print("Error contacting server:", e)
+        return False, []
 
 class InspectRoutePlanner(Component):
     """Reads a route from disk and returns it as the desired route."""
-    def __init__(self, bounding_box, frame : str = 'start'):
-
-
-
-        if frame == 'start':
-                self.route = Route(frame=ObjectFrameEnum.START,points=waypoints.tolist())
-        elif frame == 'global':
-            self.route = Route(frame=ObjectFrameEnum.GLOBAL,points=waypoints.tolist())
-        elif frame == 'cartesian':
-            self.route = Route(frame=ObjectFrameEnum.ABSOLUTE_CARTESIAN,points=waypoints.tolist())
+    def __init__(self, geofence_area, frame : str = 'start'):
+        # if frame == 'start':
+        #     self.route = Route(frame=ObjectFrameEnum.START,points=waypoints.tolist())
+        # elif frame == 'global':
+        #     self.route = Route(frame=ObjectFrameEnum.GLOBAL,points=waypoints.tolist())
+        # elif frame == 'cartesian':
+        #     self.route = Route(frame=ObjectFrameEnum.ABSOLUTE_CARTESIAN,points=waypoints.tolist())
+        # self.inspection_area = bounding_box
+        self.geofence_area = geofence_area
 
     def state_inputs(self):
-        return []
+        return ['mission', 'vehicle']
 
     def state_outputs(self) -> List[str]:
         return ['route']
@@ -109,12 +133,24 @@ class InspectRoutePlanner(Component):
     def rate(self):
         return 1.0
 
-    def update(self, state):
-        if state.mission == "IDLE":
-            return []
-        elif state.mission == "NAV":
-            pass
-        elif state.mission == "INSPECT":
-            pass
-        return self.route
+    def update(self, mission, vehicle):
+        if mission == "IDLE":
+            points_found = False
+            points_found, pts = check_point_exists()
+            if points_found:
+                self.bounding_box = pts
+                print(self.state_list[self.index+1])
+                self.mission = self.state_list[self.index+1]
+                self.index += 1
+                print("CHANGING STATES", self.mission)
+                self.start = [vehicle.pose.x,vehicle.pose.y]
+            self.circle_center = [(self.inspection_area[0][0]+self.inspection_area[1][0])/2, (self.inspection_area[0][1]+self.inspection_area[1][1])/2]
+            self.radius = ((self.inspection_area[0][0]+self.inspection_area[1][0])**2 + (self.inspection_area[0][1]+self.inspection_area[1][1])**2)**0.5/2
+            self.inspection_route = max_visible_arc(self.circle_center, self.radius, geofence_area)
+        elif mission == "NAV":
+            planner.rrt_final_point(self.inspection_route[0])
+        elif mission == "INSPECT":
+            planner.route_as_input(self.inspection_route)
+        elif mission == "FINISH":
+            planner.rrt_final_point(self.start)
     
