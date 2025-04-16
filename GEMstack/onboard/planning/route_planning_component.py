7ff0895477dd929d16af51b366025caa6538b053
pokkali2 <pokkali2@illinois.edu>
2025-04-16 08:01:23 -0500

    Integrate RRT star

--- GEMstack/onboard/planning/route_planning_component.py
+++ GEMstack/onboard/planning/route_planning_component.py
@@ -1,11 +1,13 @@
 import os
-from typing import List
+from typing import Dict, List
 
 import numpy as np
 from GEMstack.onboard.component import Component
+from GEMstack.state.agent import AgentState
 from GEMstack.state.all import AllState
 from GEMstack.state.physical_object import ObjectFrameEnum
 from GEMstack.state.route import PlannerEnum, Route
+from .rrt_star import RRTStar
 
 
 
@@ -13,6 +15,8 @@ class RoutePlanningComponent(Component):
     """Reads a route from disk and returns it as the desired route."""
     def __init__(self):
         print("Route Planning Component init")
+        self.planner = None
+        self.route = None
         
     def state_inputs(self):
         return ["all"]
@@ -24,25 +28,39 @@ class RoutePlanningComponent(Component):
         return 10.0
 
     def update(self, state: AllState):
-        print("Route Planner's mission:", state.mission_plan)
-        print("Vehicle x:", state.vehicle.pose.x)
-        print("Vehicle y:", state.vehicle.pose.y)
-        print("Vehicle yaw:", state.vehicle.pose.yaw)
-        if state.mission_plan.planner_type == PlannerEnum.PARKING:
-            print("Parking mode")
-        elif state.mission_plan.planner_type == PlannerEnum.RRT_STAR:
-            print("RRT mode")
+        # print("Route Planner's mission:", state.mission_plan.planner_type.value)
+        # print("type of mission plan:", type(PlannerEnum.RRT_STAR))
+        # print("Route Planner's mission:", state.mission_plan.planner_type.value == PlannerEnum.RRT_STAR.value)
+        # print("Route Planner's mission:", state.mission_plan.planner_type.value == PlannerEnum.PARKING.value)
+        # print("Mission plan:", state.mission_plan)
+        # print("Vehicle x:", state.vehicle.pose.x)
+        # print("Vehicle y:", state.vehicle.pose.y)
+        # print("Vehicle yaw:", state.vehicle.pose.yaw)
+        if state.mission_plan.planner_type.value == PlannerEnum.PARKING.value:
+            print("I am in PARKING mode")
+            # Return a route after doing some processing based on mission plan REMOVE ONCE OTHER PLANNERS ARE IMPLEMENTED
+            base_path = os.path.dirname(__file__)
+            file_path = os.path.join(base_path, "../../knowledge/routes/forward_15m_extra.csv")
+        
+            waypoints = np.loadtxt(file_path, delimiter=',', dtype=float)
+            if waypoints.shape[1] == 3:
+                    waypoints = waypoints[:,:2]
+            print("waypoints", waypoints)
+            self.route = Route(frame=ObjectFrameEnum.START,points=waypoints.tolist())
+        elif state.mission_plan.planner_type.value == PlannerEnum.RRT_STAR.value:
+            print("I am in RRT mode")
+            start = (state.vehicle.pose.x+1, state.vehicle.pose.y+1)
+            goal = (state.mission_plan.goal_x, state.mission_plan.goal_y) #When we implement kinodynamic, we need to include target_yaw also
+            x_bounds = (0,20)
+            y_bounds = (0,20)
+            step_size = 1.0
+            max_iter = 2000
+            occupancy_grid = np.zeros((20, 20), dtype=int) 
+            occupancy_grid[5:10, 5:10] = 1
+            self.planner = RRTStar(start, goal, x_bounds, y_bounds, max_iter=max_iter, step_size=step_size, vehicle_width=1, occupancy_grid=occupancy_grid)
+            rrt_resp = self.planner.plan()
+            self.route = Route(frame=ObjectFrameEnum.START, points=rrt_resp)
         else:
             print("Unknown mode")
         
-        ## Return a route after doing some processing based on mission plan REMOVE ONCE OTHER PLANNERS ARE IMPLEMENTED
-        base_path = os.path.dirname(__file__)
-        file_path = os.path.join(base_path, "../../knowledge/routes/forward_15m.csv")
-    
-        waypoints = np.loadtxt(file_path, delimiter=',', dtype=float)
-        if waypoints.shape[1] == 3:
-                waypoints = waypoints[:,:2]
-        print("waypoints", waypoints)
-        route = Route(frame=ObjectFrameEnum.START,points=waypoints.tolist())
-        print("route", route)
-        return route
+        return self.route
