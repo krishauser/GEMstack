18418b30ef5f752c729458e32dfb2cd6d0e9fa48
aadarshhegde <ahegd2@illinois.edu>
2025-04-22 22:18:38 -0500

    adding more integration changes

---
+++ GEMstack/state/mission_plan.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from ..utils.serialization import register
+from .route import PlannerEnum
+
+@dataclass
+@register
+class MissionPlan:
+    goal_x: float
+    goal_y: float
+    goal_orientation: float
+    planner_type : PlannerEnum = PlannerEnum.RRT_STAR
+    # other mission-specific parameters can be added here
+
+
+    
