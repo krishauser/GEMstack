5074569dbd209eb723ebb92c971dbbc7553e3ed0
Nikita Mashchenko <mashchenko1@icloud.com>
2025-04-27 17:51:22 -0500

    feat: server + frontend setup

---
+++ frontend/api/summon.ts
@@ -0,0 +1,17 @@
+import { LngLatLike } from "mapbox-gl";
+
+const summon = async (lng: number, lat: number) => {
+  return fetch("http://localhost:8000/api/summon", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      lon: lng,
+      lat,
+    }),
+  });
+};
+
+export { summon };
