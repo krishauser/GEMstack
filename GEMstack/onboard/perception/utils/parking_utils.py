28dc5a7c83c4c4896189c1f52c2fca976f8e9995
leizhenyu-lzy <lzy20190501@gmail.com>
2025-05-01 16:59:59 -0500

    delete useless functions

--- GEMstack/onboard/perception/utils/parking_utils.py
+++ GEMstack/onboard/perception/utils/parking_utils.py
@@ -5,43 +5,6 @@ from scipy.spatial import ConvexHull
 from .constants import *
 
 
-def clickPoints(imgPath=None, numPoints=4):
-    clicked_pts = []
-
-    def mouse_callback(event, x, y, flags, param):
-        if event == cv2.EVENT_LBUTTONDOWN and len(param) < numPoints:
-            param.append((x, y))
-            print(f"Point {len(param)}: ({x}, {y})")
-
-    if imgPath:
-        img = cv2.imread(imgPath, flags=cv2.IMREAD_COLOR)
-    else:
-        img = np.ones((600, 800, 3), dtype=np.uint8) * 255
-
-    cv2.namedWindow("Click Points")
-    cv2.setMouseCallback("Click Points", mouse_callback, clicked_pts)
-
-    while True:
-        img_copy = img.copy()
-
-        for pt in clicked_pts:
-            cv2.circle(img_copy, pt, 5, (0, 0, 255), -1)
-
-        if len(clicked_pts) == numPoints:
-            cv2.polylines(img_copy, [np.array(clicked_pts)], isClosed=True, color=(0, 255, 0), thickness=2)
-
-        cv2.imshow("Click Points", img_copy)
-
-        key = cv2.waitKey(10)
-        if key == 27 or key == ord("q"):  # ESC
-            break
-        elif key == ord('r'):
-            clicked_pts.clear()
-        
-    cv2.destroyAllWindows()
-    return np.array(clicked_pts, dtype=np.float32)
-
-
 def distPoint2LineAB(p, a, b):
     pa = p - a
     pb = p - b
