b8fc27dbd7ceb73433e4bf5ed8add53e535626c9
leizhenyu-lzy <lzy20190501@gmail.com>
2025-05-02 13:49:15 -0500

    fix bug

--- Fusion/FindParkingLot.py
+++ Fusion/FindParkingLot.py
@@ -100,10 +100,10 @@ def findAllCandidateParkingLot(cornerPts, angleStepDegree=5, positionStrideMeter
     
     refAngleDegree = getMaxLenEdgeAngleDegree(cornerPts)
     
-    for angleDegree in np.arange(-90, 90, angleStepDegree):
-        rect = ((0, 0), (GEM_E4_LENGTH, GEM_E4_WIDTH), float(angleDegree))
+    # for angleDegree in np.arange(-90, 90, angleStepDegree):
+    #     rect = ((0, 0), (GEM_E4_LENGTH, GEM_E4_WIDTH), float(angleDegree))
     
-    for angleDegree in np.arange(refAngleDegree, refAngleDegree+1):
+    for angleDegree in np.arange(refAngleDegree, refAngleDegree+5, angleStepDegree):
         rect = ((0, 0), (GEM_E4_LENGTH, GEM_E4_WIDTH), float(angleDegree))
         carBox = cv2.boxPoints(rect)  # 车体角点，以原点为中心
         
@@ -126,6 +126,9 @@ def findMaxLenEdgePoints(cornerPts):
         
         if tempLen > maxLen:
             maxPt1, maxPt2 = pt1, pt2
+            maxLen = tempLen
+    
+    print(f"MaxLen: {maxLen}, pt1: {maxPt1}, pt2: {maxPt2}")
     return maxPt1, maxPt2
 
 
@@ -146,11 +149,9 @@ def drawCarPose(img, center, angleDegree, color=(0, 0, 255), scale=100):
 def visualizeCandidateCarPoses(cornerPts, candidates, bestPose=None, scale=100):
     img = np.zeros((1000, 1000, 3), dtype=np.uint8)
 
-    # 画停车位边框
     parking_polygon = np.int32(cornerPts * scale)
     cv2.polylines(img, [parking_polygon], isClosed=True, color=(0, 255, 0), thickness=2)
 
-    # 画所有合法停车姿态
     for (x, y, angle) in candidates:
         drawCarPose(img, (x, y), angle, color=(100, 100, 255), scale=scale)
     
@@ -165,7 +166,7 @@ def visualizeCandidateCarPoses(cornerPts, candidates, bestPose=None, scale=100):
 def scoreAndSortCandidates(candidates, cornerPts):
     scoredCandidates = []
     for pose in candidates:
-        score = calculateCandidateScore(pose, cornerPts)  # 越小越好 → 负值越大越好
+        score = calculateCandidateScore(pose, cornerPts)
         scoredCandidates.append((pose, score))
 
     scoredCandidates.sort(key=lambda x: x[1], reverse=True)
@@ -180,44 +181,18 @@ def scoreAndSortCandidates(candidates, cornerPts):
 
 
 if __name__ == "__main__":
-    # cornerPts = np.array([
-    #     [8,9],
-    #     [4,5],
-    #     [1,15],
-    #     [5,9],
-    # ]).astype(np.float32)  # any order
-    
-    # cornerPts = np.random.randint(
-    #     low=1,
-    #     high=10,
-    #     size=(4,2)
-    # ).astype(np.float32)
-    
-    # cornerPts = clickPoints(
-    #     imgPath = os.path.join(
-    #         os.path.dirname(__file__),
-    #         r"./Pics/FrontCam.png"
-    #     )
-    # )/100
-    
-    # print(f"\
-    #     cornerPts = np.array([\n\
-    #         [{cornerPts[0,0]},{cornerPts[0,1]}],\n\
-    #         [{cornerPts[1,0]},{cornerPts[1,1]}],\n\
-    #         [{cornerPts[2,0]},{cornerPts[2,1]}],\n\
-    #         [{cornerPts[3,0]},{cornerPts[3,1]}],\n\
-    #     ]).astype(np.float32)  # any order \
-    # ")
     
     cornerPts = np.array([
-        [14.52, -3.55],
-        [11.10, -3.39],
-        [14.56, -8.41],
-        [17.82, -8.65],
+        [14.52-10, -3.55+10],
+        [11.10-10, -3.39+10],
+        [14.56-10, -8.41+10],
+        [17.82-10, -8.65+10],
     ])
     
     
     candidates = findAllCandidateParkingLot(cornerPts)
+    
+    
     
     
     
@@ -256,90 +231,5 @@ if __name__ == "__main__":
 
 
 
-# def checkFitWithMinAreaRect(pts:np.ndarray, rectLongMin=GEM_E4_LENGTH, rectShortMin=GEM_E4_WIDTH):
-    
-#     def getRotatedRectBox(center, size, angleDegree):
-#         """
-#         给定中心坐标、尺寸和角度，返回矩形4个顶点
-#         center: (x, y)
-#         size: (width, height)
-#         angleDegree: 逆时针旋转角度（OpenCV风格）
-#         """
-#         rect = (center, size, angleDegree)
-#         box = cv2.boxPoints(rect)  # 得到4个点
-#         return box  # 转换为整数像素点或保留float点
-    
-#     # 外接矩形
-#     outRect = cv2.minAreaRect(pts)
-#     (centerX, centerY), (width, length), rotation = outRect
-#     print(f"rotation: {rotation}")
-#     # OpenCV 返回的 rotation 单位是 角度，范围为 [-90°, 0°]
-#     # -90°	矩形长边在 Y 轴方向
-#     # 0°	长边在 X 轴方向
 
-#     SwitchOrder = False
-#     if width > length:
-#         outBoxLong, outBoxShort = width, length
-#     else:
-#         outBoxLong, outBoxShort = length, width
-#         SwitchOrder = True
-#         print("SwitchOrder")
-
-#     canFit = True
-#     if SwitchOrder is True:
-#         rotation += 90
-    
-
-#     # judge fitting
-#     if outBoxLong < rectLongMin:
-#         print(f"CAN NOT FIT : outBoxLong{outBoxLong} < rectLongMin{rectLongMin}")
-#         canFit = False
-#     if outBoxShort < rectShortMin:
-#         print(f"CAN NOT FIT : outBoxShort{outBoxShort} < rectShortMin{rectShortMin}")
-#         canFit = False
-
-#     centerXY = (centerX, centerY)
-    
-    
-#     outerBoxPts = getRotatedRectBox(centerXY, (outBoxLong, outBoxShort), rotation)
-#     innerBoxPts = getRotatedRectBox(centerXY, (rectLongMin, rectShortMin), rotation)
-    
-#     return canFit, centerXY, rotation, innerBoxPts, outerBoxPts
-
-
-# if __name__ == "__main__":
-    # cornerPts = np.array([
-    #     [8,10],
-    #     [3,10],
-    #     [5,5],
-    #     [1,5],
-
-    # ]).astype(np.float32)  # any order
-
-    # cornerPts = np.random.randint(
-    #     low=1,
-    #     high=10,
-    #     size=(4,2)
-    # ).astype(np.float32)
-    
-    # print(cornerPts)
-
-
-    # canFit, center, rotation, boxInner, boxOuter = checkFitWithMinAreaRect(cornerPts)
-    
-    # print(canFit, rotation)
-
-    # img = np.zeros((600, 600, 3), dtype=np.uint8)
-    # ptsInt = np.int32(cornerPts * 40)  # 放大方便看
-    # boxIntInner = np.int32(boxInner * 40)
-    # boxIntOuter = np.int32(boxOuter * 40)
-
-    # cv2.polylines(img, [ptsInt], isClosed=True, color=(0,255,0), thickness=2)
-    # cv2.polylines(img, [boxIntInner], isClosed=True, color=(255,0,0), thickness=2)
-    # cv2.polylines(img, [boxIntOuter], isClosed=True, color=(0,0,255), thickness=2)
-
-    # cv2.imshow("fit check", img)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    
     
