9f41ecc87aa85890ba0356e1771f8396324308fb
michalj1 <michalj2@illinois.edu>
2025-05-05 09:59:03 -0500

    Remove unnecessary files

--- GEMstack/offboard/calibration/run_img2pc.bash
+++
@@ -1,9 +0,0 @@
-root='/mnt/GEMstack'
-
-python3 $root/GEMstack/offboard/calibration/img2pc.py \
-    --img_path $root/data/calib1/img/fl/fl16.png \
-    --pc_path $root/data/calib1/pc/ouster16.npz \
-    --pc_transform_path $root/GEMstack/knowledge/calibration/gem_e4_ouster.yaml \
-    --img_intrinsics_path $root/GEMstack/knowledge/calibration/gem_e4_oak_in.yaml \
-    --n_features 4 \
-    --out_path $root/GEMstack/knowledge/calibration/gem_e4_oak.yaml 
