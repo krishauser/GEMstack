7b50576c64db743deb22dc303b05b300a9fa31ce
KenC1014 <kenken4016@gmail.com>
2025-05-08 00:20:47 -0500

    include newest changes from perception team

--- GEMstack/state/agent.py
+++ GEMstack/state/agent.py
@@ -12,12 +12,13 @@ class AgentEnum(Enum):
     PEDESTRIAN = 3
     BICYCLIST = 4
 
+
 class AgentActivityEnum(Enum):
     STOPPED = 0         # standing pedestrians, parked cars, etc. No need to predict motion.
     MOVING = 1          # standard motion.  Predictions will be used here
     FAST = 2            # indicates faster than usual motion, e.g., runners.
     UNDETERMINED = 3    # unknown activity
-    STANDING = 4        # standing 
+
 
 @dataclass
 @register
