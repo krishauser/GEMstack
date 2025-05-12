defe862a13d4a4836debb101f7e0ec719a2c698e
Tianyu Ren <142737618+Ren990420@users.noreply.github.com>
2025-05-12 13:44:35 -0500

    Update __init__.py
    Removed ObstacleState

--- GEMstack/state/__init__.py
+++ GEMstack/state/__init__.py
@@ -11,13 +11,12 @@ __all__ = ['PhysicalObject','ObjectPose','ObjectFrameEnum',
            'Obstacle',
            'Sign',
            'AgentState','AgentEnum','AgentActivityEnum',
+           'ObstacleMaterialEnum','ObstacleStateEnum',
            'SceneState',
-           'ObstacleState','ObstacleMaterialEnum','ObstacleStateEnum',
            'VehicleIntent','VehicleIntentEnum',
            'AgentIntent',
            'EntityRelationEnum','EntityRelation','EntityRelationGraph',
-           'MissionEnum','MissionObjective', 'MissionPlan',
-           'PlannerEnum',
+           'MissionEnum','MissionObjective',
            'Route',
            'PredicateValues',
            'AllState']
@@ -26,7 +25,6 @@ from .trajectory import Path,Trajectory
 from .vehicle import VehicleState,VehicleGearEnum
 from .roadgraph import Roadgraph, RoadgraphLane, RoadgraphCurve, RoadgraphRegion, RoadgraphCurveEnum, RoadgraphLaneEnum, RoadgraphRegionEnum, RoadgraphSurfaceEnum, RoadgraphConnectionEnum
 from .obstacle import Obstacle, ObstacleMaterialEnum, ObstacleStateEnum
-from .obstacle import Obstacle, ObstacleMaterialEnum
 from .sign import Sign, SignEnum, SignalLightEnum, SignState
 from .roadmap import Roadmap
 from .agent import AgentState, AgentEnum, AgentActivityEnum
@@ -35,7 +33,6 @@ from .intent import VehicleIntent, VehicleIntentEnum
 from .agent_intent import AgentIntent
 from .relations import EntityRelation, EntityRelationEnum, EntityRelationGraph
 from .mission import MissionEnum,MissionObjective
-from .route import Route, PlannerEnum
-from .mission import MissionObjective
+from .route import Route
 from .predicates import PredicateValues
 from .all import AllState
