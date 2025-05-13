fbc3677df3d7c4a837a92c8d53994ad56188cff9
maoyifei <maoyifei2013605@163.com>
2025-05-13 14:39:22 -0500

    bash file that runs all different kinds of simulation together

---
+++ GEMstack/onboard/planning/run_sim.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <scene_file>"
+    exit 1
+fi
+
+SCENE_PATH=$1
+
+echo "Using scene: $SCENE_PATH"
+
+# Inject scene path into the template and create a temporary YAML config
+env SCENE_PATH="$SCENE_PATH" envsubst < ./launch/parking_template.yaml > ./launch/parking_generated.yaml
+
+# Run the simulation
+python3 main.py --variant=reeds_shepp ./launch/parking_testing.yaml
+
+python3 main.py --variant=approx_reeds_shepp ./launch/parking_testing.yaml
+
+python3 main.py --variant=euclidian ./launch/parking_testing.yaml
+
+## To run this file with selected scne, ./run_sim.sh ../scenes/parking/demo_2.yaml
