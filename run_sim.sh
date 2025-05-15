b5e85f64592fe02457e108f5fefbd275636578d6
Simon <shimonkato2@gmail.com>
2025-05-14 23:34:29 -0500

    Added testing scenes

--- run_sim.sh
+++ run_sim.sh
@@ -6,13 +6,12 @@ fi
 SCENE_PATH=$1
 echo "Using scene: $SCENE_PATH"
 # Inject scene path into the template and create a temporary YAML config
-env SCENE_PATH="$SCENE_PATH" envsubst < ./launch/parking_testing.yaml > ./launch/parking_generated.yaml
+# env SCENE_PATH="$SCENE_PATH" envsubst < ./launch/parking_testing.yaml > ./launch/parking_generated.yaml
 # Run the simulation
-python3 main.py --variant=reeds_shepp ./launch/parking_generated.yaml
-python3 main.py --variant=approx_reeds_shepp ./launch/parking_generated.yaml
-python3 main.py --variant=euclidian ./launch/parking_generated.yaml
-## To run this file with selected scne, ./run_sim.sh ../scenes/parking/demo_2.yaml
-
+python3 main.py --variant=reeds_shepp --simulator.scene=scenes/parking/demo_$SCENE_PATH.yaml ./launch/parking_generated.yaml
+python3 main.py --variant=approx_reeds_shepp --simulator.scene=scenes/parking/demo_$SCENE_PATH.yaml ./launch/parking_generated.yaml
+python3 main.py --variant=euclidean --simulator.scene=scenes/parking/demo_$SCENE_PATH.yaml ./launch/parking_generated.yaml
+## To run this file with selected scne, ./run_sim.sh 1
 
 
 
